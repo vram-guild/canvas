43112aa15b4cebfc32ac657bf1cdedeb31b30fe5
grondag <grondag@gmail.com>
2019-02-22 22:14:23 -0800

    Port indigo as baseline code

---
+++ src/main/java/grondag/canvas/RendererImpl.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2016, 2017, 2018 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package grondag.canvas;
+
+import java.util.HashMap;
+
+import net.fabricmc.fabric.api.client.model.fabric.MaterialFinder;
+import net.fabricmc.fabric.api.client.model.fabric.MeshBuilder;
+import net.fabricmc.fabric.api.client.model.fabric.RenderMaterial;
+import net.fabricmc.fabric.api.client.model.fabric.Renderer;
+import grondag.canvas.RenderMaterialImpl.Value;
+import grondag.canvas.mesh.MeshBuilderImpl;
+import net.minecraft.util.Identifier;
+
+/**
+ * The Fabric default renderer implementation. Supports all
+ * features defined in the API except shaders and offers no special materials.
+ */
+public class RendererImpl implements Renderer {
+    public static final RendererImpl INSTANCE = new RendererImpl();
+    
+    public static final RenderMaterialImpl.Value MATERIAL_STANDARD = (Value) INSTANCE.materialFinder().find();
+    
+    static {
+        INSTANCE.registerMaterial(RenderMaterial.MATERIAL_STANDARD, MATERIAL_STANDARD);
+    }
+    
+    private final HashMap<Identifier, RenderMaterial> materialMap = new HashMap<>();
+    
+    private RendererImpl() { };
+
+    @Override
+    public MeshBuilder meshBuilder() {
+        return new MeshBuilderImpl();
+    }
+  
+    @Override
+    public MaterialFinder materialFinder() {
+        return new RenderMaterialImpl.Finder();
+    }
+
+    @Override
+    public RenderMaterial materialById(Identifier id) {
+        return materialMap.get(id);
+    }
+
+    @Override
+    public boolean registerMaterial(Identifier id, RenderMaterial material) {
+        if(materialMap.containsKey(id))
+            return false;
+        // cast to prevent acceptance of impostor implementations
+        materialMap.put(id, material);
+        return true;
+    }
+}
---
+++ src/main/java/grondag/canvas/mixin/MixinChunkRenderTask.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016, 2017, 2018 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package grondag.canvas.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import grondag.canvas.accessor.AccessSafeWorldView;
+import grondag.canvas.render.TerrainRenderContext;
+import net.minecraft.client.render.chunk.ChunkRenderTask;
+import net.minecraft.client.world.SafeWorldView;
+
+@Mixin(ChunkRenderTask.class)
+public abstract class MixinChunkRenderTask {
+    @Shadow private SafeWorldView worldView;
+    
+    /**
+     * The block view reference is voided when {@link ChunkRenderTask#getAndInvalidateWorldView()} is called during
+     * chunk rebuild, but we need it and it is harder to make reliable, non-invasive changes there.
+     * So we capture the block view before the reference is voided and send it to the renderer. <p>
+     * 
+     * We also store a reference to the renderer in the view to avoid doing thread-local lookups for each block.
+     */
+    @Inject(at = @At("HEAD"), method = "getAndInvalidateWorldView")
+    private void chunkDataHook(CallbackInfoReturnable<SafeWorldView> info) {
+        final SafeWorldView blockView = worldView;
+        if(blockView != null) {
+            final TerrainRenderContext renderer  = TerrainRenderContext.POOL.get();
+            renderer.setBlockView(blockView);
+            ((AccessSafeWorldView)blockView).fabric_setRenderer(renderer);
+        }
+    }
+}
