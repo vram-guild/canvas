32e36685c87ab3b4680343756af1bd3e5f4a0190
grondag <grondag@gmail.com>
2019-04-12 18:23:13 -0700

    Minor fixes

--- src/main/java/grondag/canvas/mixin/MixinFogState.java
+++ src/main/java/grondag/canvas/mixin/MixinFogState.java
@@ -18,7 +18,6 @@ package grondag.canvas.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
