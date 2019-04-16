d1801113160f4c01b9711c01a01f99979f75b32f
grondag <grondag@gmail.com>
2019-04-16 08:38:25 -0700

    Don't disable lightmap for non-shader first-person rendering

--- src/main/java/grondag/canvas/mixin/MixinFirstPersonRenderer.java
+++ src/main/java/grondag/canvas/mixin/MixinFirstPersonRenderer.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2019 grondag
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
 package grondag.canvas.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,10 +32,9 @@ import net.minecraft.util.math.BlockPos;
 public class MixinFirstPersonRenderer {
     @Shadow private MinecraftClient client;
     
-    @Inject(at = @At("HEAD"), method = "applyLightmap", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "applyLightmap")
     private void onApplyLightmap(CallbackInfo ci) {
         AbstractClientPlayerEntity player = this.client.player;
         ItemRenderContext.playerLightMapIndex(this.client.world.getLightmapIndex(new BlockPos(player.x, player.y + (double)player.getStandingEyeHeight(), player.z), 0));
-        ci.cancel();
     }
 }
