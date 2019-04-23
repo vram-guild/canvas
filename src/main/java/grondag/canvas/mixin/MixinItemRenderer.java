3c008264d9238aad24f1a80f354f4cec6465b403
grondag <grondag@gmail.com>
2019-04-22 20:00:28 -0700

    Splice in the old item context for now

--- src/main/java/grondag/canvas/mixin/MixinItemRenderer.java
+++ src/main/java/grondag/canvas/mixin/MixinItemRenderer.java
@@ -24,7 +24,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import grondag.canvas.Configurator;
 import grondag.canvas.apiimpl.rendercontext.ItemRenderContext;
+import grondag.canvas.apiimpl.rendercontext.legacy.ItemRenderContextOld;
 import grondag.canvas.draw.TessellatorExt;
 import grondag.canvas.material.ShaderContext;
 import grondag.canvas.varia.GuiLightingHelper;
@@ -43,15 +45,17 @@ public abstract class MixinItemRenderer {
     @Shadow
     protected abstract void renderQuads(BufferBuilder bufferBuilder, List<BakedQuad> quads, int color, ItemStack stack);
 
-    @Shadow
-    protected ItemColorMap colorMap;
+    @Shadow protected ItemColorMap colorMap;
     
     private ItemRenderContext context;
+    private ItemRenderContextOld oldContext;
+    
     private final TessellatorExt tessellatorExt = (TessellatorExt) Tessellator.getInstance();
 
     @Inject(method = "<init>*", at = @At("RETURN"), require = 1)
     private void afterInit(CallbackInfo ci) {
         context = new ItemRenderContext(colorMap);
+        oldContext = new ItemRenderContextOld(colorMap);
     }
     
     /**
@@ -60,28 +64,45 @@ public abstract class MixinItemRenderer {
      */
     @Inject(at = @At("HEAD"), method = "renderItemAndGlow")
     private void hookRenderItemAndGlow(ItemStack stack, BakedModel model, CallbackInfo ci) {
-        if (!model.isBuiltin() && stack.hasEnchantmentGlint()) {
-            context.enchantmentStack = stack;
+        if(Configurator.enableItemRender) {
+            if (!model.isBuiltin() && stack.hasEnchantmentGlint()) {
+                context.enchantmentStack = stack;
+            }
+        } else {
+            if (stack.hasEnchantmentGlint() && !((DynamicBakedModel) model).isVanillaAdapter()) {
+                oldContext.enchantmentStack = stack;
+            }
         }
     }
 
     @Inject(at = @At("HEAD"), method = "renderModel", cancellable = true)
     private void hookRenderModel(BakedModel model, int color, ItemStack stack, CallbackInfo ci) {
         DynamicBakedModel dynamicModel = (DynamicBakedModel) model;
-        // PERF: redirect most of the enables so we don't have to change state here each time
-        GuiLightingHelper.suspend();
-        context.renderModel(dynamicModel, color, stack);
-        GuiLightingHelper.resume();
-        ci.cancel();
+        if(Configurator.enableItemRender) {
+            // PERF: redirect most of the enables so we don't have to change state here each time
+            GuiLightingHelper.suspend();
+            context.renderModel(dynamicModel, color, stack);
+            GuiLightingHelper.resume();
+            ci.cancel();
+        } else {
+            if (!dynamicModel.isVanillaAdapter()) {
+                oldContext.renderModel(dynamicModel, color, stack, this::renderQuads);
+                ci.cancel();
+            }
+        }
     }
     
     @Inject(at = @At("HEAD"), method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/BakedModel;Lnet/minecraft/client/render/model/json/ModelTransformation$Type;Z)V")
     private void onRenderItem(ItemStack itemStack, BakedModel bakedModel, ModelTransformation.Type type, boolean flag, CallbackInfo ci) {
-        tessellatorExt.canvas_context(ShaderContext.ITEM_WORLD);
+        if(Configurator.enableItemRender) {
+            tessellatorExt.canvas_context(ShaderContext.ITEM_WORLD);
+        }
     }
     
     @Inject(at = @At("HEAD"), method = "renderGuiItemModel")
     private void onRenderGuiItemModel(ItemStack itemStack, int int_1, int int_2, BakedModel bakedModel, CallbackInfo ci) {
-        tessellatorExt.canvas_context(ShaderContext.ITEM_GUI);
+        if(Configurator.enableItemRender) {
+            tessellatorExt.canvas_context(ShaderContext.ITEM_GUI);
+        }
     }
 }
