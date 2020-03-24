eaf55edbf672e72c5ec581c247adefac8eabf80c
Grondag <grondag@gmail.com>
2020-03-24 08:39:02 -0700

    Improve frustum test locality

--- src/main/java/grondag/canvas/render/CanvasFrustum.java
+++ src/main/java/grondag/canvas/render/CanvasFrustum.java
@@ -12,13 +12,23 @@ import net.fabricmc.api.Environment;
 import grondag.canvas.chunk.BuiltRenderRegion;
 import grondag.canvas.mixinterface.Matrix4fExt;
 
+/**
+ * Plane equation derivations based on:
+ * "Fast Extraction of Viewing Frustum Planes from the World- View-Projection Matrix"
+ * Gill Gribb, Klaus Hartmann
+ * https://www.gamedevs.org/uploads/fast-extraction-viewing-frustum-planes-from-world-view-projection-matrix.pdf
+ *
+ * AABB test method based on work by Ville Miettinen
+ * as described in Real-Time Rendering, Fourth Edition (Page 971). CRC Press.
+ * Abbey, Duane C.; Haines, Eric; Hoffman, Naty.
+ */
 @Environment(EnvType.CLIENT)
 public class CanvasFrustum extends Frustum {
 	private final Matrix4f mvpMatrix = new Matrix4f();
 
 	private Frustum testFrustum;
 
-	private final Plane[] planes = {new Plane(), new Plane(), new Plane(), new Plane(), new Plane(), new Plane()};
+	private final float[] planes = new float[6 * PLANE_STRIDE];
 
 	public CanvasFrustum() {
 		super(dummyMatrix(), dummyMatrix());
@@ -35,7 +45,7 @@ public class CanvasFrustum extends Frustum {
 		mvpMatrix.multiply(projectionMatrix);
 		mvpMatrix.multiply(modelMatrix);
 
-		extractPlanes(planes, (Matrix4fExt)(Object) mvpMatrix);
+		extractPlanes();
 
 		testFrustum = new Frustum(modelMatrix, projectionMatrix);
 
@@ -55,38 +65,21 @@ public class CanvasFrustum extends Frustum {
 		final float cy = region.cameraRelativeCenterY;
 		final float cz = region.cameraRelativeCenterZ;
 
-		return planeAABBIntersect(cx, cy, cz, planes[0]) != OUTSIDE //  L
-				&& planeAABBIntersect(cx, cy, cz, planes[1]) != OUTSIDE //  R
-				&& planeAABBIntersect(cx, cy, cz, planes[2]) != OUTSIDE // TOP
-				&& planeAABBIntersect(cx, cy, cz, planes[3]) != OUTSIDE // BOTT
-				&& planeAABBIntersect(cx, cy, cz, planes[4]) != OUTSIDE // NEAR
-				&& planeAABBIntersect(cx, cy, cz, planes[5]) != OUTSIDE; // FAR
+		return planeAABBIntersect(cx, cy, cz, PLANE_NEAR) != OUTSIDE
+				&& planeAABBIntersect(cx, cy, cz, PLANE_LEFT) != OUTSIDE
+				&& planeAABBIntersect(cx, cy, cz, PLANE_RIGHT) != OUTSIDE
+				&& planeAABBIntersect(cx, cy, cz, PLANE_TOP) != OUTSIDE
+				&& planeAABBIntersect(cx, cy, cz, PLANE_BOTTOM) != OUTSIDE;
+		//				&& planeAABBIntersect(cx, cy, cz, PLANE_FAR) != OUTSIDE;
 	}
 
-	static final int OUTSIDE = 0;
-	static final int INSIDE = 1;
-	static final int INTERSECTING = 2;
-
-	private int planeAABBIntersect(float cx, float cy, float cz, Plane plane) {
-		// c = center
-		//		final float c = (b max + b min)/2;
-		//		final float cx = (float) (box.x1 + 8 - cameraX);
-		//		final float cy = (float) (box.y1 + 8 - cameraY);
-		//		final float cz = (float) (box.z1 + 8 - cameraZ);
-
-		// h = half diagonal
-		//		float h = (final b max − final b min)/2;
-		//		final float hx = (float) ((box.x2 - box.x1) / 2);
-		//		final float hy = (float) ((box.y2 - box.y1) / 2);
-		//		final float hz = (float) ((box.z2 - box.z1) / 2);
-
-		// PERF: can hard-code the above is always the same
-
-		//final float e = hx|nx| + hy|ny| + hz|nz|;
-		final float e = plane.extent;
-		//final float e = 8 * Math.abs(plane.a) + 8 * Math.abs(plane.b) + 8 * Math.abs(plane.c);
-
-		final float s = cx * plane.a + cy * plane.b + cz * plane.c + plane.d;
+	private int planeAABBIntersect(float cx, float cy, float cz, int planeOffset) {
+		final float[] planes = this.planes;
+		final float e = planes[planeOffset + PLANE_EXTENT];
+		final float s = cx * planes[planeOffset + PLANE_NORMAL_X]
+				+ cy * planes[planeOffset + PLANE_NORMAL_Y]
+						+ cz * planes[planeOffset + PLANE_NORMAL_Z]
+								+ planes[planeOffset + PLANE_DISTANCE];
 
 		if(s - e > 0) {
 			return OUTSIDE;
@@ -97,66 +90,86 @@ public class CanvasFrustum extends Frustum {
 		}
 	}
 
-	static class Plane {
-		float a, b, c, d;
-		float extent;
+	private static final int OUTSIDE = 0;
+	private static final int INSIDE = 1;
+	private static final int INTERSECTING = 2;
+
+	private static final int PLANE_NORMAL_X = 0;
+	private static final int PLANE_NORMAL_Y = 1;
+	private static final int PLANE_NORMAL_Z = 2;
+	private static final int PLANE_DISTANCE = 3;
+	private static final int PLANE_EXTENT = 4;
+	private static final int PLANE_STRIDE = PLANE_EXTENT + 1;
+
+	private static final int PLANE_LEFT = 0;
+	private static final int PLANE_RIGHT = PLANE_STRIDE;
+	private static final int PLANE_TOP = PLANE_STRIDE * 2;
+	private static final int PLANE_BOTTOM = PLANE_STRIDE * 3;
+	private static final int PLANE_NEAR = PLANE_STRIDE * 4;
+	//	private static final int PLANE_FAR = PLANE_STRIDE * 5;
+
+	private void normalizePlane(int planeOffset) {
+		final float[] planes = this.planes;
+
+		float a  = planes[planeOffset + PLANE_NORMAL_X];
+		float b  = planes[planeOffset + PLANE_NORMAL_Y];
+		float c  = planes[planeOffset + PLANE_NORMAL_Z];
+		float d  = planes[planeOffset + PLANE_DISTANCE];
+
+		final float mag = -1f / (float) Math.sqrt(a * a + b * b + c * c);
+
+		a *= mag;
+		b *= mag;
+		c *= mag;
+		d *= mag;
+
+		planes[planeOffset + PLANE_NORMAL_X] = a;
+		planes[planeOffset + PLANE_NORMAL_Y] = b;
+		planes[planeOffset + PLANE_NORMAL_Z] = c;
+		planes[planeOffset + PLANE_DISTANCE] = d;
+
+		planes[planeOffset + PLANE_EXTENT] = 8 * (Math.abs(a) + Math.abs(b) + Math.abs(c));
 	}
 
-	static void normalizePlane(Plane plane){
-		final float mag = -(float) Math.sqrt(plane.a * plane.a + plane.b * plane.b + plane.c * plane.c);
-		plane.a = plane.a / mag;
-		plane.b = plane.b / mag;
-		plane.c = plane.c / mag;
-		plane.d = plane.d / mag;
+	private void extractPlanes() {
+		final Matrix4fExt matrix = (Matrix4fExt)(Object) mvpMatrix;
+		final float[] planes = this.planes;
 
-		plane.extent = 8 * (Math.abs(plane.a) + Math.abs(plane.b) + Math.abs(plane.c));
-	}
+		planes[PLANE_LEFT + PLANE_NORMAL_X] = matrix.a30() + matrix.a00();
+		planes[PLANE_LEFT + PLANE_NORMAL_Y] = matrix.a31() + matrix.a01();
+		planes[PLANE_LEFT + PLANE_NORMAL_Z] = matrix.a32() + matrix.a02();
+		planes[PLANE_LEFT + PLANE_DISTANCE] = matrix.a33() + matrix.a03();
 
-	static void extractPlanes(
-			Plane[] planes,
-			Matrix4fExt matrix)
-	{
-		// Left clipping plane
-		planes[0].a = matrix.a30() + matrix.a00();
-		planes[0].b = matrix.a31() + matrix.a01();
-		planes[0].c = matrix.a32() + matrix.a02();
-		planes[0].d = matrix.a33() + matrix.a03();
+		planes[PLANE_RIGHT + PLANE_NORMAL_X] = matrix.a30() - matrix.a00();
+		planes[PLANE_RIGHT + PLANE_NORMAL_Y] = matrix.a31() - matrix.a01();
+		planes[PLANE_RIGHT + PLANE_NORMAL_Z] = matrix.a32() - matrix.a02();
+		planes[PLANE_RIGHT + PLANE_DISTANCE] = matrix.a33() - matrix.a03();
 
-		// Right clipping plane
-		planes[1].a = matrix.a30() - matrix.a00();
-		planes[1].b = matrix.a31() - matrix.a01();
-		planes[1].c = matrix.a32() - matrix.a02();
-		planes[1].d = matrix.a33() - matrix.a03();
+		planes[PLANE_TOP + PLANE_NORMAL_X] = matrix.a30() - matrix.a10();
+		planes[PLANE_TOP + PLANE_NORMAL_Y] = matrix.a31() - matrix.a11();
+		planes[PLANE_TOP + PLANE_NORMAL_Z] = matrix.a32() - matrix.a12();
+		planes[PLANE_TOP + PLANE_DISTANCE] = matrix.a33() - matrix.a13();
 
-		// Top clipping plane
-		planes[2].a = matrix.a30() - matrix.a10();
-		planes[2].b = matrix.a31() - matrix.a11();
-		planes[2].c = matrix.a32() - matrix.a12();
-		planes[2].d = matrix.a33() - matrix.a13();
+		planes[PLANE_BOTTOM + PLANE_NORMAL_X] = matrix.a30() + matrix.a10();
+		planes[PLANE_BOTTOM + PLANE_NORMAL_Y] = matrix.a31() + matrix.a11();
+		planes[PLANE_BOTTOM + PLANE_NORMAL_Z] = matrix.a32() + matrix.a12();
+		planes[PLANE_BOTTOM + PLANE_DISTANCE] = matrix.a33() + matrix.a13();
 
-		// Bottom clipping plane
-		planes[3].a = matrix.a30() + matrix.a10();
-		planes[3].b = matrix.a31() + matrix.a11();
-		planes[3].c = matrix.a32() + matrix.a12();
-		planes[3].d = matrix.a33() + matrix.a13();
+		planes[PLANE_NEAR + PLANE_NORMAL_X] = matrix.a30() + matrix.a20();
+		planes[PLANE_NEAR + PLANE_NORMAL_Y] = matrix.a31() + matrix.a21();
+		planes[PLANE_NEAR + PLANE_NORMAL_Z] = matrix.a32() + matrix.a22();
+		planes[PLANE_NEAR + PLANE_DISTANCE] = matrix.a33() + matrix.a23();
 
-		// Near clipping plane
-		planes[4].a = matrix.a30() + matrix.a20();
-		planes[4].b = matrix.a31() + matrix.a21();
-		planes[4].c = matrix.a32() + matrix.a22();
-		planes[4].d = matrix.a33() + matrix.a23();
+		//		planes[PLANE_FAR + PLANE_NORMAL_X] = matrix.a30() - matrix.a20();
+		//		planes[PLANE_FAR + PLANE_NORMAL_Y] = matrix.a31() - matrix.a21();
+		//		planes[PLANE_FAR + PLANE_NORMAL_Z] = matrix.a32() - matrix.a22();
+		//		planes[PLANE_FAR + PLANE_DISTANCE] = matrix.a33() - matrix.a23();
 
-		// Far clipping plane
-		planes[5].a = matrix.a30() - matrix.a20();
-		planes[5].b = matrix.a31() - matrix.a21();
-		planes[5].c = matrix.a32() - matrix.a22();
-		planes[5].d = matrix.a33() - matrix.a23();
-
-		normalizePlane(planes[0]);
-		normalizePlane(planes[1]);
-		normalizePlane(planes[2]);
-		normalizePlane(planes[3]);
-		normalizePlane(planes[4]);
-		normalizePlane(planes[5]);
+		normalizePlane(PLANE_LEFT);
+		normalizePlane(PLANE_RIGHT);
+		normalizePlane(PLANE_TOP);
+		normalizePlane(PLANE_BOTTOM);
+		normalizePlane(PLANE_NEAR);
+		//		normalizePlane(PLANE_FAR);
 	}
 }
