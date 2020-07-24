969b5356d0538bd0b6496b066a9b298c36057bc7
Grondag <grondag@gmail.com>
2020-07-24 08:10:04 -0700

    Nuke LambDynLights dep and use reflection instead.  Prevents gradle from polluting downstream environments.

--- src/main/java/grondag/canvas/compat/LambDynLightsHolder.java
+++ src/main/java/grondag/canvas/compat/LambDynLightsHolder.java
@@ -14,6 +14,9 @@
  ******************************************************************************/
 package grondag.canvas.compat;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Method;
 import java.util.function.Consumer;
 
 import net.minecraft.client.render.WorldRenderer;
@@ -23,14 +26,40 @@ import net.fabricmc.loader.api.FabricLoader;
 import grondag.canvas.CanvasMod;
 
 public class LambDynLightsHolder {
-	public static Consumer<WorldRenderer> updateAll = init();
+	private static boolean warnInit = true;
 
-	static Consumer<WorldRenderer> init() {
+	public static Consumer<WorldRenderer> updateAll = wr -> {};
+
+	static {
 		if (FabricLoader.getInstance().isModLoaded("lambdynlights")) {
-			CanvasMod.LOG.info("Found LambDynLights - compatibility hook enabled");
-			return LambDynLightsHelper.updateAll();
-		} else {
-			return w -> {};
+
+			final MethodHandles.Lookup lookup = MethodHandles.lookup();
+
+			try {
+				final Class<?> clazz = Class.forName("me.lambdaurora.lambdynlights.LambDynLights");
+				final Method getInstance = clazz.getDeclaredMethod("get");
+				final Object instance =  getInstance.invoke(null);
+
+				final Method update = clazz.getDeclaredMethod("updateAll", WorldRenderer.class);
+				final MethodHandle updateHandler = lookup.unreflect(update);
+				final MethodHandle boundReUpdateHandler = updateHandler.bindTo(instance);
+
+				updateAll = wr -> {
+					try  {
+						boundReUpdateHandler.invokeExact(wr);
+					} catch (final Throwable e) {
+						if (warnInit) {
+							CanvasMod.LOG.warn("Unable to call LambDynLights updateAll hook due to exception:", e);
+							CanvasMod.LOG.warn("Subsequent errors will be suppressed");
+							warnInit = false;
+						}
+					}
+				};
+
+				CanvasMod.LOG.info("Found LambDynLights - compatibility hook enabled");
+			} catch (final Exception e)  {
+				CanvasMod.LOG.warn("Unable to find LambDynLights render hook due to exception:", e);
+			}
 		}
 	}
 }
