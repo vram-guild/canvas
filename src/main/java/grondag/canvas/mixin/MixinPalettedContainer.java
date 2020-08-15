051ed708961096db09889c3fc196f5d845f9af44
Grondag <grondag@gmail.com>
2020-08-15 10:57:52 -0700

    Prevent crashes in ./gradlew runClient  closes #78

--- src/main/java/grondag/canvas/mixin/MixinPalettedContainer.java
+++ src/main/java/grondag/canvas/mixin/MixinPalettedContainer.java
@@ -3,9 +3,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -28,13 +28,13 @@ import grondag.canvas.terrain.ChunkPaletteCopier.PaletteCopy;
 
 @Mixin(PalettedContainer.class)
 public abstract class MixinPalettedContainer<T> implements PalettedContainerExt {
-	@Shadow private T field_12935;
+	@Shadow private T defaultValue;
 	@Shadow protected PackedIntegerArray data;
 	@Shadow private Palette<T> palette;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public PaletteCopy canvas_paletteCopy() {
-		return ChunkPaletteCopier.captureCopy((Palette<BlockState>) palette, data, (BlockState)field_12935);
+		return ChunkPaletteCopier.captureCopy((Palette<BlockState>) palette, data, (BlockState)defaultValue);
 	}
 }
