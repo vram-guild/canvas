5d25e84d10cc9b62bd7ca25fb2468388f01a63e7
Grondag <grondag@gmail.com>
2020-12-30 06:28:13 -0800

    Carve out pipeline vertex API

--- src/main/resources/assets/canvas/shaders/internal/material_main.vert
+++ src/main/resources/assets/canvas/shaders/internal/material_main.vert
@@ -5,8 +5,8 @@
 #include canvas:shaders/internal/flags.glsl
 #include frex:shaders/api/vertex.glsl
 #include frex:shaders/api/sampler.glsl
-#include canvas:shaders/internal/diffuse.glsl
 #include canvas:shaders/internal/program.glsl
+#include canvas:shaders/pipeline/standard_vertex.glsl
 
 #include canvas:apitarget
 
@@ -24,11 +24,11 @@ void _cv_endVertex(inout frx_VertexData data, in int cv_programId) {
 
 void main() {
 	frx_VertexData data = frx_VertexData(
-	gl_Vertex,
-	in_uv,
-	in_color,
-	(in_normal_flags.xyz - 127.0) / 127.0,
-	in_lightmap.rg * 0.00390625 + 0.03125
+		gl_Vertex,
+		in_uv,
+		in_color,
+		(in_normal_flags.xyz - 127.0) / 127.0,
+		in_lightmap.rg * 0.00390625 + 0.03125
 	);
 
 	// Adding +0.5 prevents striping or other strangeness in flag-dependent rendering
@@ -38,8 +38,15 @@ void main() {
 	_cv_setupProgram();
 
 	int cv_programId = _cv_vertexProgramId();
+
+	// material shaders go first
 	_cv_startVertex(data, cv_programId);
 
+	// pipeline shader goes after
+	// will typically do writes or set out variables here that use pre-transform values
+	frx_startPipelineVertex(data);
+
+	// map texture coordinates
 	if (_cvu_atlas[_CV_SPRITE_INFO_TEXTURE_SIZE] != 0.0) {
 		float spriteIndex = in_material.x;
 		// for sprite atlas textures, convert from normalized (0-1) to interpolated coordinates
@@ -59,28 +66,13 @@ void main() {
 
 	data.spriteUV = _cv_textureCoord(data.spriteUV, 0);
 
-	vec4 viewCoord = gl_ModelViewMatrix * data.vertex;
-	gl_ClipVertex = viewCoord;
-	gl_FogFragCoord = length(viewCoord.xyz);
-
+	// apply transforms
 	//data.normal *= gl_NormalMatrix;
 	data.vertex = gl_ModelViewProjectionMatrix * data.vertex;
 
-	gl_Position = data.vertex;
-
+	// post-transform material shaders
 	_cv_endVertex(data, cv_programId);
 
-	_cvv_texcoord = data.spriteUV;
-	_cvv_color = data.color;
-	_cvv_normal = data.normal;
-
-#if DIFFUSE_SHADING_MODE != DIFFUSE_MODE_NONE
-	_cvv_diffuse = _cv_diffuse(_cvv_normal);
-#endif
-
-	#if AO_SHADING_MODE != AO_MODE_NONE
-	_cvv_ao = in_lightmap.b / 255.0;
-#endif
-
-	_cvv_lightcoord = data.light;
+	// pipeline shader handles additional writes/out variables
+	frx_endtPipelineVertex(data);
 }
--- src/main/resources/assets/frex/shaders/api/vertex.glsl
+++ src/main/resources/assets/frex/shaders/api/vertex.glsl
@@ -3,15 +3,16 @@
 ******************************************************/
 
 /*
- * Varying variables for generic use. Shader authors are
- * encouraged to exhaust these before creating new custom varyings.
+ * Varying variables for generic use in material shaders. Material
+ * shader authors are encouraged to exhaust these before creating new
+ * custom out variables.
  *
  * This is necessary because custom shaders may be consolidated
  * into a single shader with logic controlled via uniforms or vertex data.
  * This is done either to reduce draw calls or as a way to achieve
  * sorted translucency with mixed custom shaders.
  *
- * If we do not reuse varying variable, then three bad things can happen:
+ * If we do not reuse these variables, then three bad things can happen:
  *   1) Naming conflicts (could be avoided with care)
  *   2) Exceed hardware/driver limits
  *   3) Wasteful interpolation if unused varyings aren't stripped by the compiler.
@@ -19,6 +20,10 @@
  * Authors do not need to worry about conflicting usage of these variables
  * by other shaders in the same compilation - only a single pair of custom
  * vertex/fragment shaders will be active for a single polygon.
+ *
+ * Note that pipeline shader devs should NOT use these.  There will only
+ * ever be a single pipeline active at any time - piplines can define as
+ * many out variables as needed, within reason.
  */
 varying vec4 frx_var0;
 varying vec4 frx_var1;
@@ -27,7 +32,7 @@ varying vec4 frx_var3;
 
 
 /*
- * Usage in API:
+ * Usage in API for material shaders:
  * 	  void frx_startVertex(inout frx_VertexData data)
  * 	  void frx_endVertex(inout frx_VertexData data)
  *
@@ -41,73 +46,92 @@ varying vec4 frx_var3;
  * retrieving post-transform values to set up varying variable.
  *
  * The exception to the above is vertex. See notes below.
+ *
+ *
+ * Usage in API for pipeline shaders:
+ * 	  void frx_startPipelineVertex(inout frx_VertexData data)
+ * 	  void frx_endPipelineVertex(inout frx_VertexData data)
+ *
+ * The comments that apply to material shaders also apply here.
+ * Both calls will come immediate after the material shader runs.
+ *
+ * The renderer will marshal all data into the structure. (Vertex
+ * formats are managed entirely by the renderer.) Before the 2nd call,
+ * the renderer will transform UV coordinates for atlas textures from normalized
+ * to mapped and also apply matrix transforms to vertex and normal.
+ *
+ * The pipeline is responsible for ALL WRITES.  The renderer will
+ * not update fog, clip, color, nor any other frameebuffer attachments.
+ * Outputs that need pre-transform values should happen in frx_startPipelineVertex
+ * and outpus that need post-transform values should happen in frx_endPipelineVertex.
+ *
+ * The renderer WILL set out variables for the interpolated values
+ * presented in frx_startFragment but these are not directly exposed
+ * outside of that method. Any other out variables the pipeline needs must
+ * be set in one of these two methods.
  */
 struct frx_VertexData {
-/*
- * Vertex position. Transformation during cv_startVertex
- * is the primary means for achieving animation effects.
- * Remember that normals must be transformed separately!
- *
- * Will be world space during cv_startVertex and in
- * screen space during cv_endVertex.
- *
- * Note that unlike other attributes, changes made in
- * cv_endVertex will NOT be used.  This limitation may
- * be removed in a future enhancement.
- */
+	/*
+	 * Vertex position. Transformation during cv_startVertex
+	 * is the primary means for achieving animation effects.
+	 * Remember that normals must be transformed separately!
+	 *
+	 * Will be world space during cv_startVertex and in
+	 * screen space during cv_endVertex.
+	 */
 	vec4 vertex;
 
-/*
- * The primary texture coordinate for the vertex.
- * These are raw coordinates during cv_startVertex
- * and will modified by the active texture matrix in
- * cv_endVertex if one is in effect (usually not.)
- *
- * IMPORTANT: currently these are atlas coordinates.
- * This will soon be changed to 0-1 coordinates within
- * the texture itself. This will generally be more useful
- * for in-shader effects that rely on position relative
- * to texture bounds.
- */
+	/*
+	 * The primary texture coordinate for the vertex.
+	 * These are raw coordinates during cv_startVertex
+	 * and will modified by the active texture matrix in
+	 * cv_endVertex if one is in effect (usually not.)
+	 *
+	 * IMPORTANT: currently these are atlas coordinates.
+	 * This will soon be changed to 0-1 coordinates within
+	 * the texture itself. This will generally be more useful
+	 * for in-shader effects that rely on position relative
+	 * to texture bounds.
+	 */
 	vec2 spriteUV;
 
-/*
- * RGB vertex color, with alpha.
- * Usage of alpha is controlled by the material blend mode:
- * 		if cutout is enabled, fragment will be discarded (see: api/material.glsl)
- * 		if SHADER_PASS != SHADER_PASS_SOLID fragment will be blended using the alpha channel
- *		in other cases (solid pass rendering) alpha is ignored and could be used for other purposes
- *
- * Canvas does not modify vertex alpha values given from the model
- * in the solid render pass, even though the data is not used. This
- * means mod authors can use that value for other purposes in the shader.
- *
- * Vertex color is not transformed by the renderer.
- */
+	/*
+	 * RGB vertex color, with alpha.
+	 * Usage of alpha is controlled by the material blend mode:
+	 * 		if cutout is enabled, fragment will be discarded (see: api/material.glsl)
+	 * 		if SHADER_PASS != SHADER_PASS_SOLID fragment will be blended using the alpha channel
+	 *		in other cases (solid pass rendering) alpha is ignored and could be used for other purposes
+	 *
+	 * Canvas does not modify vertex alpha values given from the model
+	 * in the solid render pass, even though the data is not used. This
+	 * means mod authors can use that value for other purposes in the shader.
+	 *
+	 * Vertex color is not transformed by the renderer.
+	 */
 	vec4 color;
 
-/*
- * Vertex normal. Transformation during cv_startVertex
- * is the primary means for achieving animation effects.
- * Transforming normal in addition to vertex is important
- * for correct lighting.
- *
- * Will be in world space during cv_startVertex and in
- * screen space during cv_endVertex.
- */
+	/*
+	 * Vertex normal. Transformation during cv_startVertex
+	 * is the primary means for achieving animation effects.
+	 * Transforming normal in addition to vertex is important
+	 * for correct lighting.
+	 *
+	 * Will be in world space during cv_startVertex and in
+	 * screen space during cv_endVertex.
+	 */
 	vec3 normal;
 
-/*
- * Block and sky light intensity for this vertex as 0 to 1 values.
- * Block is X and sky is Y.
- *
- * Depending on the context or lighting model in effect,
- * this may not be populated or used.
- *
- * Recommendation is to avoid using or modifying this value
- * unless VANILLA_LIGHTING = TRUE.
- *
- * The emissive flag is generally a better alternative.
- */
+	/*
+	 * Block and sky light intensity for this vertex as 0 to 1 values.
+	 * Block is X and sky is Y.
+	 *
+	 * Depending on the context or lighting model in effect,
+	 * this may not be populated or used.
+	 *
+	 * Recommendation is to avoid using or modifying this value
+	 * unless VANILLA_LIGHTING = TRUE.
+	 *
+	 * The emissive flag is generally a better alternative.
+	 */
 	vec2 light;
 };
