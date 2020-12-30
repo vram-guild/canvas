222edf6f4f27f35f014dec637aba5fe9208c5348
Grondag <grondag@gmail.com>
2020-12-30 08:25:04 -0800

    Camera->view, move transform stuff from world to view
    Non-breaking due to includes

--- src/main/resources/assets/frex/shaders/api/world.glsl
+++ src/main/resources/assets/frex/shaders/api/world.glsl
@@ -1,10 +1,15 @@
 #include canvas:shaders/internal/world.glsl
 #include canvas:shaders/internal/flags.glsl
+#include frex:shaders/api/view.glsl
 
 /******************************************************
   frex:shaders/api/world.glsl
 
   Utilities for querying world information.
+
+  Some transformation-related methods have been moved
+  to view.glsl, which is currently included to prevent
+  breaks until shaders can be updated.
 ******************************************************/
 
 /*
@@ -126,74 +131,6 @@ bool frx_isThundering() {
  */
 bool frx_isSkyDarkened() {
 	return frx_bitValue(_cvu_world_flags, _CV_FLAG0_IS_SKY_DARKENED) == 1.0;
-}
-
-/*
- * World coordinates for model space origin in the current invocation.
- * Add this to vertex position to get world position.
- */
-vec3 frx_modelOriginWorldPos() {
-	return _cvu_model_origin;
-}
-
-/*
- * Vertex coordinates are relative to the camera and include model transformations
- * as well as camera rotation and translation via MatrixStack.
- * The GL view matrix will be the identity matrix. (the default state in world render)
- * Used for most per-frame renders (entities, block entities, etc.)
- */
-#define MODEL_ORIGIN_ENTITY 0
-
-/*
- * Vertex coordinates are relative to the camera and include model translation, scaling
- * and billboard rotation plus camera translation via matrix stack but not camera rotation.
- * The GL view matrix will include camera rotation.
- * Used for particle rendering.
- */
-#define MODEL_ORIGIN_PARTICLE 1
-
-/*
- * Vertex coordinate are raw model coordinates.
- * Will need a view matrix update per draw.
- * Currently not used.
- */
-#define MODEL_ORIGIN_MODEL 2
-
-/*
- * Vertex coordinates are relative to a world region and
- * include all model transformations.
- * GL view matrix must be updated for both camera rotation and offset.
- * Used in terrain rendering. Canvas regions may be 16x16 or 256x256.
- */
-#define MODEL_ORIGIN_REGION 3
-
-/*
- * Vertex coordinates are relative to the screen.
- * Intended for GUI rendering.
- * Currently not used.
- */
-#define MODEL_ORIGIN_SCREEN 4
-
-/**
- * Describes how vertex coordinates relate to world and camera geometry.
- * Will be one of the MODEL_ORIGIN_ constants defined above.
- *
- * Except as noted, GL state is always assumed to have the projection
- * matrix set and view matrix set to identity. Some types have additional
- * view matrix transformation.
- */
-int frx_modelOriginType() {
-	return _cvu_model_origin_type;
-}
-
-/**
- * Converts world space normals to coordinate space of incoming vertex data.
- * Entity render, for example, has camera rotation already baked in to
- * vertex data and so a pure world normal vector can be transformed
- * with this to be consistent with vertex normals.
- */
-mat3 frx_normalModelMatrix() {
-	return _cvu_normal_model_matrix;
 }
 
 bool frx_testCondition(int conditionIndex) {
