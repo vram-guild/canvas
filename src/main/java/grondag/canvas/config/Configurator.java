56ddf0c0a9e748b7a10726293b8bbcad684d7e9d
Grondag <grondag@gmail.com>
2021-01-06 09:36:04 -0800

    Add straggler pipeline file

---
+++ src/main/java/grondag/canvas/config/Configurator.java
@@ -0,0 +1,220 @@
+/*
+ *  Copyright 2019, 2020 grondag
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License.  You may obtain a copy
+ *  of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+
+package grondag.canvas.config;
+
+import static grondag.canvas.config.ConfigManager.DEFAULTS;
+
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+
+import net.minecraft.util.math.MathHelper;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+import grondag.canvas.perf.LagFinder;
+import grondag.canvas.pipeline.config.PipelineConfig;
+import grondag.canvas.pipeline.config.PipelineLoader;
+
+@Environment(EnvType.CLIENT)
+public class Configurator {
+	static final ConfigEntryBuilder ENTRY_BUILDER = ConfigEntryBuilder.create();
+	public static String pipelineId = DEFAULTS.pipelineId;
+	public static FogMode fogMode = DEFAULTS.fogMode;
+	public static boolean blendFluidColors = DEFAULTS.blendFluidColors;
+	public static boolean enableBloom = DEFAULTS.enableBloom;
+	public static float bloomIntensity = DEFAULTS.bloomIntensity;
+	public static float bloomScale = DEFAULTS.bloomScale;
+	public static boolean wavyGrass = DEFAULTS.wavyGrass;
+	public static int handheldLightRadius = DEFAULTS.handheldLightRadius;
+	public static boolean lightmapNoise = DEFAULTS.lightmapNoise;
+	public static DiffuseMode diffuseShadingMode = DEFAULTS.diffuseShadingMode;
+	public static boolean lightSmoothing = DEFAULTS.lightSmoothing;
+	public static AoMode aoShadingMode = DEFAULTS.aoShadingMode;
+	public static boolean moreLightmap = DEFAULTS.moreLightmap;
+	public static int maxLightmapDelayFrames = DEFAULTS.maxLightmapDelayFrames;
+	public static boolean semiFlatLighting = DEFAULTS.semiFlatLighting;
+	public static boolean batchedChunkRender = DEFAULTS.batchedChunkRender;
+	public static boolean preventDepthFighting = DEFAULTS.preventDepthFighting;
+	public static boolean clampExteriorVertices = DEFAULTS.clampExteriorVertices;
+	public static boolean fixLuminousBlockShading = DEFAULTS.fixLuminousBlockShading;
+	public static boolean terrainSetupOffThread = DEFAULTS.terrainSetupOffThread;
+	public static boolean cullEntityRender = DEFAULTS.cullEntityRender;
+	public static boolean greedyRenderThread = DEFAULTS.greedyRenderThread;
+	public static boolean forceJmxModelLoading = DEFAULTS.forceJmxModelLoading;
+	public static boolean reduceResolutionOnMac = DEFAULTS.reduceResolutionOnMac;
+	public static boolean vertexControlMode = DEFAULTS.vertexControlMode;
+	public static int staticFrustumPadding = DEFAULTS.staticFrustumPadding;
+	public static int dynamicFrustumPadding = DEFAULTS.dynamicFrustumPadding;
+	public static boolean cullParticles = DEFAULTS.cullParticles;
+	public static boolean shaderDebug = DEFAULTS.shaderDebug;
+	public static boolean lightmapDebug = DEFAULTS.lightmapDebug;
+	public static boolean conciseErrors = DEFAULTS.conciseErrors;
+	public static boolean logMachineInfo = DEFAULTS.logMachineInfo;
+	public static boolean logGlStateChanges = DEFAULTS.logGlStateChanges;
+	public static boolean debugNativeMemoryAllocation = DEFAULTS.debugNativeMemoryAllocation;
+	public static boolean safeNativeMemoryAllocation = DEFAULTS.safeNativeMemoryAllocation;
+	public static boolean enablePerformanceTrace = DEFAULTS.enablePerformanceTrace;
+	public static boolean debugOcclusionRaster = DEFAULTS.debugOcclusionRaster;
+	public static boolean debugOcclusionBoxes = DEFAULTS.debugOcclusionBoxes;
+	public static boolean traceOcclusionOutcomes = DEFAULTS.traceOcclusionOutcomes;
+	public static boolean traceOcclusionEdgeCases = DEFAULTS.traceOcclusionEdgeCases;
+	public static boolean enableBufferDebug = DEFAULTS.enableBufferDebug;
+	public static boolean enableLifeCycleDebug = DEFAULTS.enableLifeCycleDebug;
+	public static boolean logMissingUniforms = DEFAULTS.logMissingUniforms;
+	public static boolean logMaterials = DEFAULTS.logMaterials;
+	static boolean logRenderLagSpikes = DEFAULTS.logRenderLagSpikes;
+	static int renderLagSpikeFps = DEFAULTS.renderLagSpikeFps;
+
+	public static LagFinder lagFinder = createLagFinder();
+
+	static LagFinder createLagFinder() {
+		return logRenderLagSpikes ? LagFinder.create(() -> 1000000000 / renderLagSpikeFps) : LagFinder.DUMMMY;
+	}
+
+	//    @LangKey("config.acuity_fancy_fluids")
+	//    @Comment({"Enable fancy water and lava rendering.",
+	//        " This feature is currently work in progress and has no visible effect if enabled."})
+	public static boolean fancyFluids = false;
+	static boolean reload = false;
+	static boolean hdLightmaps = DEFAULTS.hdLightmaps;
+	static boolean enableVao = DEFAULTS.enableVao;
+
+	public static boolean hdLightmaps() {
+		return false;
+	}
+
+	public static boolean enableVao() {
+		return false;
+	}
+
+	static void readFromConfig(ConfigData config) {
+		pipelineId = config.pipelineId;
+
+		if (pipelineId == null || pipelineId.isEmpty() || PipelineLoader.get(pipelineId) == null) {
+			pipelineId = PipelineConfig.DEFAULT_ID.toString();
+		}
+
+		fogMode = config.fogMode;
+		blendFluidColors = config.blendFluidColors;
+		enableBloom = config.enableBloom;
+		bloomIntensity = config.bloomIntensity;
+		bloomScale = config.bloomScale;
+		wavyGrass = config.wavyGrass;
+		handheldLightRadius = config.handheldLightRadius;
+
+		shaderDebug = config.shaderDebug;
+		maxLightmapDelayFrames = config.maxLightmapDelayFrames;
+		moreLightmap = config.moreLightmap;
+
+		hdLightmaps = config.hdLightmaps;
+		lightmapNoise = config.lightmapNoise;
+		diffuseShadingMode = config.diffuseShadingMode;
+		lightSmoothing = config.lightSmoothing;
+		aoShadingMode = config.aoShadingMode;
+		semiFlatLighting = config.semiFlatLighting;
+
+		batchedChunkRender = config.batchedChunkRender;
+		//        disableVanillaChunkMatrix = config.disableVanillaChunkMatrix;
+		preventDepthFighting = config.preventDepthFighting;
+		clampExteriorVertices = config.clampExteriorVertices;
+		fixLuminousBlockShading = config.fixLuminousBlockShading;
+		terrainSetupOffThread = config.terrainSetupOffThread;
+		safeNativeMemoryAllocation = config.safeNativeMemoryAllocation;
+		enableVao = config.enableVao;
+		cullEntityRender = config.cullEntityRender;
+		greedyRenderThread = config.greedyRenderThread;
+		forceJmxModelLoading = config.forceJmxModelLoading;
+		reduceResolutionOnMac = config.reduceResolutionOnMac;
+		vertexControlMode = config.vertexControlMode;
+		dynamicFrustumPadding = MathHelper.clamp(config.dynamicFrustumPadding, 0, 20);
+		staticFrustumPadding = MathHelper.clamp(config.staticFrustumPadding, 0, 30);
+		cullParticles = config.cullParticles;
+
+		lightmapDebug = config.lightmapDebug;
+		conciseErrors = config.conciseErrors;
+		logMachineInfo = config.logMachineInfo;
+		logGlStateChanges = config.logGlStateChanges;
+		debugNativeMemoryAllocation = config.debugNativeMemoryAllocation;
+		enablePerformanceTrace = config.enablePerformanceTrace;
+		debugOcclusionBoxes = config.debugOcclusionBoxes;
+		debugOcclusionRaster = config.debugOcclusionRaster;
+		traceOcclusionEdgeCases = config.traceOcclusionEdgeCases;
+		traceOcclusionOutcomes = config.traceOcclusionOutcomes;
+		enableBufferDebug = config.enableBufferDebug;
+		enableLifeCycleDebug = config.enableLifeCycleDebug;
+		logMissingUniforms = config.logMissingUniforms;
+		logMaterials = config.logMaterials;
+		logRenderLagSpikes = config.logRenderLagSpikes;
+		renderLagSpikeFps = MathHelper.clamp(config.renderLagSpikeFps, 30, 120);
+		lagFinder = createLagFinder();
+	}
+
+	static void writeToConfig(ConfigData config) {
+		config.pipelineId = pipelineId;
+		config.fogMode = fogMode;
+		config.blendFluidColors = blendFluidColors;
+		config.enableBloom = enableBloom;
+		config.bloomIntensity = bloomIntensity;
+		config.bloomScale = bloomScale;
+		config.wavyGrass = wavyGrass;
+		config.handheldLightRadius = handheldLightRadius;
+
+		config.shaderDebug = shaderDebug;
+		config.maxLightmapDelayFrames = maxLightmapDelayFrames;
+
+		config.hdLightmaps = hdLightmaps;
+		config.lightmapNoise = lightmapNoise;
+		config.diffuseShadingMode = diffuseShadingMode;
+		config.lightSmoothing = lightSmoothing;
+		config.aoShadingMode = aoShadingMode;
+		config.moreLightmap = moreLightmap;
+		config.semiFlatLighting = semiFlatLighting;
+
+		config.batchedChunkRender = batchedChunkRender;
+		config.preventDepthFighting = preventDepthFighting;
+		config.clampExteriorVertices = clampExteriorVertices;
+		config.fixLuminousBlockShading = fixLuminousBlockShading;
+		config.terrainSetupOffThread = terrainSetupOffThread;
+		config.safeNativeMemoryAllocation = safeNativeMemoryAllocation;
+		config.enableVao = enableVao;
+		config.cullEntityRender = cullEntityRender;
+		config.greedyRenderThread = greedyRenderThread;
+		config.forceJmxModelLoading = forceJmxModelLoading;
+		config.reduceResolutionOnMac = reduceResolutionOnMac;
+		config.vertexControlMode = vertexControlMode;
+		config.staticFrustumPadding = staticFrustumPadding;
+		config.dynamicFrustumPadding = dynamicFrustumPadding;
+		config.cullParticles = cullParticles;
+
+		config.lightmapDebug = lightmapDebug;
+		config.conciseErrors = conciseErrors;
+		config.logMachineInfo = logMachineInfo;
+		config.logGlStateChanges = logGlStateChanges;
+		config.debugNativeMemoryAllocation = debugNativeMemoryAllocation;
+		config.enablePerformanceTrace = enablePerformanceTrace;
+		config.debugOcclusionBoxes = debugOcclusionBoxes;
+		config.debugOcclusionRaster = debugOcclusionRaster;
+		config.traceOcclusionOutcomes = traceOcclusionOutcomes;
+		config.traceOcclusionEdgeCases = traceOcclusionEdgeCases;
+		config.enableBufferDebug = enableBufferDebug;
+		config.enableLifeCycleDebug = enableLifeCycleDebug;
+		config.logMissingUniforms = logMissingUniforms;
+		config.logMaterials = logMaterials;
+		config.logRenderLagSpikes = logRenderLagSpikes;
+		config.renderLagSpikeFps = renderLagSpikeFps;
+	}
+}
