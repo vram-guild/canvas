73a637523ec7be605c09b7877d7aadc309e9b076
spiralhalo <re.nanashi95@gmail.com>
2021-04-08 18:16:47 +0700

    Flash profiler overlay red when lag spike occurs

--- src/main/java/grondag/canvas/perf/Timekeeper.java
+++ src/main/java/grondag/canvas/perf/Timekeeper.java
@@ -11,13 +11,14 @@ import net.minecraft.util.Util;
 
 public abstract class Timekeeper {
 
+	private static long threshold;
+
 	public abstract void startFrame(String token);
 	public abstract void swap(String token);
 	public abstract void complete();
 
 	private static class Active extends Timekeeper {
 		private long start;
-		private long threshold;
 		private String currentStep;
 		private Object2LongOpenHashMap<String> stepElapsed;
 		private ObjectArrayList<String> steps;
@@ -39,7 +40,6 @@ public abstract class Timekeeper {
 			// Setting up container is done in start of frame 0
 			// This prevents multiple setup calls from config reload with multiple config vars
 			if (frameSinceReload == 0) {
-				threshold = 1000000000L / Configurator.renderLagSpikeFps;
 				stepElapsed = new Object2LongOpenHashMap<>();
 				steps = new ObjectArrayList<>();
 			}
@@ -84,6 +84,7 @@ public abstract class Timekeeper {
 			if (instance == DEACTIVATED) {
 				instance = new Active();
 			}
+			threshold = 1000000000L / Configurator.renderLagSpikeFps;
 			final Active active = (Active)instance;
 			active.reload();
 		}
@@ -100,19 +101,27 @@ public abstract class Timekeeper {
 
 		int i = 0;
 		for (String step:active.steps) {
-			renderTime(step, active.stepElapsed.getLong(step), i++, matrices, fontRenderer);
+			final long elapsed = active.stepElapsed.getLong(step);
+			final int forecolor;
+			final int backcolor;
+			if (elapsed <= threshold) {
+				forecolor = 0xFFFFFF;
+				backcolor = 0x99000000;
+			} else {
+				forecolor = 0xFFFF00;
+				backcolor = 0x99990000;
+			}
+			renderTime(step, elapsed, i++, forecolor, backcolor, matrices, fontRenderer);
 		}
 
 		matrices.pop();
 	}
 
-	final static private int render_forecolor = 0xFFFFFF;
-	final static private int render_backcolor = 0x99000000;
-	private static void renderTime(String label, long time, int i, MatrixStack matrices, TextRenderer fontRenderer) {
+	private static void renderTime(String label, long time, int i, int forecolor, int backcolor, MatrixStack matrices, TextRenderer fontRenderer) {
 		final String s = String.format("%s: %f ms", label, (float)time/1000000f);
 		final int k = fontRenderer.getWidth(s);
 		final int m = 100 + 12 * i;
-		DrawableHelper.fill(matrices, 20, m - 1, 22 + k + 1, m + 9, render_backcolor);
-		fontRenderer.draw(matrices, s, 21, m, render_forecolor);
+		DrawableHelper.fill(matrices, 20, m - 1, 22 + k + 1, m + 9, backcolor);
+		fontRenderer.draw(matrices, s, 21, m, forecolor);
 	}
 }
