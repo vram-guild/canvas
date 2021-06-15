33743e223af73a6fdba3f2cbd6e6a51ba1c911a2
grondag <>
2021-06-15 08:47:04 -0700

    Simplified iteration for skylight/shadow region visibility

--- src/main/java/grondag/canvas/terrain/occlusion/ShadowPotentiallyVisibleRegionSet.java
+++ src/main/java/grondag/canvas/terrain/occlusion/ShadowPotentiallyVisibleRegionSet.java
@@ -18,33 +18,16 @@ package grondag.canvas.terrain.occlusion;
 
 import java.util.Arrays;
 
-import it.unimi.dsi.fastutil.longs.LongArrays;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 
-import grondag.canvas.apiimpl.util.GeometryHelper;
 import grondag.canvas.terrain.region.RenderRegionIndexer;
 
 public class ShadowPotentiallyVisibleRegionSet<T extends ShadowPotentiallyVisibleRegion> implements PotentiallyVisibleRegionSet<T> {
 	private int version = 1;
 
 	private final T[] regions;
-
-	/**
-	 * Holds offsets from the sweeping plane for each indexed position within the
-	 * sweep cross-section. Used to ensure that we iterate positions that are
-	 * closest to the light. Because the skylight/shadow use an orthogonal projection,
-	 * we consider every region on the sweeping place to be equally close. So we
-	 * can iterate easily by translating these offsets through the region volume.
-	 *
-	 * <p>The address space is smaller when we are sweeping along the X or Z axis,
-	 * but to avoid waste we use a single offset array large enough for Y axis, too.
-	 */
-	private final int[] sortOrder = new int[RenderRegionIndexer.LOADED_REGION_INDEX_COUNT];
-
-	private int sortIndex = 0;
 
 	int xBase;
 	int zBase;
@@ -53,6 +36,120 @@ public class ShadowPotentiallyVisibleRegionSet<T extends ShadowPotentiallyVisibl
 		this.regions = regions;
 		assert regions.length == RenderRegionIndexer.PADDED_REGION_INDEX_COUNT;
 	}
+
+	private interface AxisIterator {
+		boolean next();
+
+		void reset();
+	}
+
+	private int x, y, z;
+
+	private final AxisIterator XPOS = new AxisIterator() {
+		@Override
+		public boolean next() {
+			if (++x < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER) {
+				return true;
+			} else {
+				reset();
+				return false;
+			}
+		}
+
+		@Override
+		public void reset() {
+			x = 0;
+		}
+	};
+
+	private final AxisIterator XNEG = new AxisIterator() {
+		@Override
+		public boolean next() {
+			if (--x >= 0) {
+				return true;
+			} else {
+				reset();
+				return false;
+			}
+		}
+
+		@Override
+		public void reset() {
+			x = RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER - 1;
+		}
+	};
+
+	private final AxisIterator YPOS = new AxisIterator() {
+		@Override
+		public boolean next() {
+			if (++y < RenderRegionIndexer.MAX_Y_REGIONS) {
+				return true;
+			} else {
+				reset();
+				return false;
+			}
+		}
+
+		@Override
+		public void reset() {
+			y = 0;
+		}
+	};
+
+	private final AxisIterator YNEG = new AxisIterator() {
+		@Override
+		public boolean next() {
+			if (--y >= 0) {
+				return true;
+			} else {
+				reset();
+				return false;
+			}
+		}
+
+		@Override
+		public void reset() {
+			y = RenderRegionIndexer.MAX_Y_REGIONS - 1;
+		}
+	};
+
+	private final AxisIterator ZPOS = new AxisIterator() {
+		@Override
+		public boolean next() {
+			if (++z < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER) {
+				return true;
+			} else {
+				reset();
+				return false;
+			}
+		}
+
+		@Override
+		public void reset() {
+			z = 0;
+		}
+	};
+
+	private final AxisIterator ZNEG = new AxisIterator() {
+		@Override
+		public boolean next() {
+			if (--z >= 0) {
+				return true;
+			} else {
+				reset();
+				return false;
+			}
+		}
+
+		@Override
+		public void reset() {
+			z = RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER - 1;
+		}
+	};
+
+	private AxisIterator primary = XPOS;
+	private AxisIterator secondary = YPOS;
+	private AxisIterator tertiary = ZPOS;
 
 	/**
 	 * Points toward the light from any point in the scene.
@@ -68,172 +165,53 @@ public class ShadowPotentiallyVisibleRegionSet<T extends ShadowPotentiallyVisibl
 	 * @param z z-axis component of light vector
 	 */
 	public void setLightVectorAndRestart(float x, float y, float z) {
-		// signs are flipped because vector is towards light and we are sweeping away from it
-		x = -x;
-		y = -y;
-		z = -z;
+		final float ax = Math.abs(x);
+		final float ay = Math.abs(y);
+		final float az = Math.abs(z);
 
-		Direction sweepDirection = Direction.byId(GeometryHelper.closestFaceFromNormal(x, y, z));
-
-		switch (sweepDirection) {
-			case EAST:
-				assert false;
-				break;
-			case NORTH:
-				assert false;
-				break;
-			case SOUTH:
-				assert false;
-				break;
-			case WEST:
-				assert false;
-				break;
-			case UP:
-				setupSweepUp(x, y, z);
-				break;
-			case DOWN:
-				setupSweepDown(x, y, z);
-				break;
-			default:
-				assert false : "Bad shadow light vector";
-				setupSweepDown(x, y, z);
-				break;
-		}
-
-		returnToStart();
-	}
-
-	final int[][] depth = new int[RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER][RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER];
-
-	void setupSweepDown(float x, float y, float z) {
-		final int[][] depth = this.depth;
-
-		int minDepth = Integer.MAX_VALUE;
-
-		for (int rx = 0; rx < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER; ++rx) {
-			int brx = (rx - RenderRegionIndexer.MAX_LOADED_CHUNK_RADIUS) << 4;
-
-			final int[] subDepth = depth[rx];
-
-			for (int rz = 0; rz < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER; ++rz) {
-				int brz = (rz - RenderRegionIndexer.MAX_LOADED_CHUNK_RADIUS) << 4;
-				int d = (int) Math.floor((brx * x + brz * z) / -y) >> 4;
-
-				if (d < minDepth) {
-					minDepth = d;
+		if (ax > ay) {
+			if (ax > az) {
+				// X primary
+				if (ay > az) {
+					// ORDER XYZ
+					primary = x > 0 ? XNEG : XPOS;
+					secondary = y > 0 ? YNEG : YPOS;
+					tertiary = z > 0 ? ZNEG : ZPOS;
+				} else {
+					// ORDER XZY
+					primary = x > 0 ? XNEG : XPOS;
+					secondary = z > 0 ? ZNEG : ZPOS;
+					tertiary = y > 0 ? YNEG : YPOS;
 				}
-
-				subDepth[rz] = d;
+			} else {
+				// Z primary, because X > Y and Z >= X
+				// ORDER ZXY
+				primary = z > 0 ? ZNEG : ZPOS;
+				secondary = x > 0 ? XNEG : XPOS;
+				tertiary = y > 0 ? YNEG : YPOS;
 			}
-		}
-
-		int n = 0;
-		int depthOffset = RenderRegionIndexer.MAX_Y_REGIONS - 1 - minDepth;
-
-		while (n < RenderRegionIndexer.LOADED_REGION_INDEX_COUNT) {
-			for (int rx = 0; rx < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER; ++rx) {
-				final int[] subDepth = depth[rx];
-
-				for (int rz = 0; rz < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER; ++rz) {
-					int ry = subDepth[rz] + depthOffset;
-
-					if (ry < RenderRegionIndexer.MAX_Y_REGIONS && ry >= 0) {
-						sortOrder[n++] = index(rx, ry, rz);
-					}
+		} else {
+			// X cannot be primary
+			if (ay > az) {
+				// Y primary, XZ order undetermined
+				if (ax > az) {
+					// ORDER YXZ
+					primary = y > 0 ? YNEG : YPOS;
+					secondary = x > 0 ? XNEG : XPOS;
+					tertiary = z > 0 ? ZNEG : ZPOS;
+				} else {
+					// ORDER YZX
+					primary = y > 0 ? YNEG : YPOS;
+					secondary = z > 0 ? ZNEG : ZPOS;
+					tertiary = x > 0 ? XNEG : XPOS;
 				}
+			} else {
+				// Z primary, because Y >= X and Z >= Y
+				// ORDER ZYX
+				primary = z > 0 ? ZNEG : ZPOS;
+				secondary = y > 0 ? YNEG : YPOS;
+				tertiary = x > 0 ? XNEG : XPOS;
 			}
-
-			--depthOffset;
-		}
-	}
-
-	void setupSweepUp(float x, float y, float z) {
-		final int[][] depth = this.depth;
-
-		int maxDepth = Integer.MIN_VALUE;
-
-		for (int rx = 0; rx < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER; ++rx) {
-			int brx = (rx - RenderRegionIndexer.MAX_LOADED_CHUNK_RADIUS) << 4;
-
-			final int[] subDepth = depth[rx];
-
-			for (int rz = 0; rz < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER; ++rz) {
-				int brz = (rz - RenderRegionIndexer.MAX_LOADED_CHUNK_RADIUS) << 4;
-				int d = Math.round((brx * x + brz * z) / -y) >> 4;
-
-				if (d > maxDepth) {
-					maxDepth = d;
-				}
-
-				subDepth[rz] = d;
-			}
-		}
-
-		int n = 0;
-		int depthOffset = RenderRegionIndexer.MAX_Y_REGIONS + 1 + maxDepth;
-
-		while (n < RenderRegionIndexer.LOADED_REGION_INDEX_COUNT) {
-			for (int rx = 0; rx < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER; ++rx) {
-				final int[] subDepth = depth[rx];
-
-				for (int rz = 0; rz < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER; ++rz) {
-					int ry = subDepth[rz] - depthOffset;
-
-					if (ry < RenderRegionIndexer.MAX_Y_REGIONS && ry >= 0) {
-						sortOrder[n++] = index(rx, ry, rz);
-					}
-				}
-			}
-
-			--depthOffset;
-		}
-	}
-
-	final long[] wideSorter = new long[RenderRegionIndexer.LOADED_REGION_INDEX_COUNT];
-
-	/** Slow but almost certainly correct.  Here for testing support. */
-	public void setLightVectorAndRestartSlowly(float x, float y, float z) {
-		// signs are flipped because vector is towards light and we are sweeping away from it
-		x = -x;
-		y = -y;
-		z = -z;
-
-		final long[] wideSorter = new long[RenderRegionIndexer.LOADED_REGION_INDEX_COUNT];
-
-		int n = 0;
-
-		for (int rx = 0; rx < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER; ++rx) {
-			int brx = (rx - RenderRegionIndexer.MAX_LOADED_CHUNK_RADIUS) << 4;
-
-			for (int rz = 0; rz < RenderRegionIndexer.MAX_LOADED_CHUNK_DIAMETER; ++rz) {
-				int brz = (rz - RenderRegionIndexer.MAX_LOADED_CHUNK_RADIUS) << 4;
-
-				for (int ry = 0; ry < RenderRegionIndexer.MAX_Y_REGIONS; ++ry) {
-					int bry = (ry << 4) - RenderRegionIndexer.Y_BLOCKPOS_OFFSET;
-					long dist = Math.round((brx * x + bry * y + brz * z + 10000f));
-
-					//assert dist > 0;
-
-					int index = index(rx, ry, rz);
-
-					long val = (dist << 20) | index;
-
-					//assert val >= 0;
-
-					wideSorter[n++] = val;
-
-					//assert val >> 20 == dist;
-					//assert (val & 0x000FFFFF) == index;
-				}
-			}
-		}
-
-		//assert n == RenderRegionIndexer.LOADED_REGION_INDEX_COUNT;
-
-		LongArrays.radixSort(wideSorter);
-
-		for (int i = 0; i < RenderRegionIndexer.LOADED_REGION_INDEX_COUNT; ++i) {
-			sortOrder[i] = (int) (wideSorter[i] & 0x000FFFFFL);
 		}
 
 		returnToStart();
@@ -301,21 +279,37 @@ public class ShadowPotentiallyVisibleRegionSet<T extends ShadowPotentiallyVisibl
 		regions[index(rx, ry, rz)] = region;
 	}
 
+	boolean complete = false;
+
 	@Override
 	public void returnToStart() {
-		sortIndex = 0;
+		primary.reset();
+		secondary.reset();
+		tertiary.reset();
+		complete = false;
 	}
 
 	@Override
 	public @Nullable T next() {
-		while (sortIndex < RenderRegionIndexer.LOADED_REGION_INDEX_COUNT) {
-			final T region = regions[sortOrder[sortIndex++]];
+		T result = null;
 
-			if (region != null) {
-				return region;
+		if (!complete) {
+			while (result == null) {
+				System.out.println(x + ", " + y + ", " + z);
+				int i = index(x, y, z);
+				result = regions[i];
+
+				if (!tertiary.next()) {
+					if (!secondary.next()) {
+						if (!primary.next()) {
+							complete = true;
+							break;
+						}
+					}
+				}
 			}
 		}
 
-		return null;
+		return result;
 	}
 }
