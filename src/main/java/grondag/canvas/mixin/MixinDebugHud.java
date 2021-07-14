25d1875a9fe2fdfbd7397959bd73e733dc5f6f08
grondag <>
2021-07-13 21:47:51 -0700

    The call overhead was real

--- src/main/java/grondag/canvas/mixin/MixinDebugHud.java
+++ src/main/java/grondag/canvas/mixin/MixinDebugHud.java
@@ -18,28 +18,126 @@ package grondag.canvas.mixin;
 
 import java.util.List;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.DebugHud;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.BufferRenderer;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Matrix4f;
 
 import grondag.canvas.buffer.GlBufferAllocator;
 import grondag.canvas.buffer.TransferBufferAllocator;
 import grondag.canvas.buffer.encoding.ArrayVertexCollector;
-import grondag.canvas.mixinterface.TextRendererExt;
-import grondag.canvas.render.gui.DrawBetterer;
 
 @Mixin(DebugHud.class)
 public class MixinDebugHud extends DrawableHelper {
 	@Shadow private TextRenderer textRenderer;
+
+	private List<String> leftList, rightList;
+
+	@Redirect(method = "renderLeftText", at = @At(value = "INVOKE", target = "Ljava/util/List;size()I"))
+	private int onGetLeftListSize(List<String> leftList) {
+		this.leftList = leftList;
+		return 0;
+	}
+
+	@Redirect(method = "renderRightText", at = @At(value = "INVOKE", target = "Ljava/util/List;size()I"))
+	private int onGetRightListSize(List<String> rightList) {
+		this.rightList = rightList;
+		return 0;
+	}
+
+	@Inject(method = "renderRightText", at = @At("RETURN"), cancellable = false, require = 1)
+	private void afterRenderRightText(MatrixStack matrixStack, CallbackInfo ci) {
+		drawLists(matrixStack);
+	}
+
+	private final BufferBuilder fillerBuffer = new BufferBuilder(4096);
+	private final VertexConsumerProvider.Immediate textBuffer = VertexConsumerProvider.immediate(new BufferBuilder(4096));
+	private static final int HEIGHT = 9;
+
+	private void drawLists(MatrixStack matrixStack) {
+		final Matrix4f matrix4f = matrixStack.peek().getModel();
+		final TextRenderer textRenderer = this.textRenderer;
+		final boolean rightToLeft = textRenderer.isRightToLeft();
+
+		fillerBuffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+
+		final int leftLimit = leftList.size();
+
+		for (int i = 0; i < leftLimit; ++i) {
+			final String string = leftList.get(i);
+
+			if (string == null || string.isEmpty()) {
+				continue;
+			}
+
+			final int top = 2 + HEIGHT * i;
+			final int x1 = 2 + textRenderer.getWidth(string) + 1;
+			final int y0 = top - 1;
+			final int y1 = top + HEIGHT - 1;
+			fillerBuffer.vertex(matrix4f, 1, y1, 0.0F).color(0x50, 0x50, 0x50, 0x90).next();
+			fillerBuffer.vertex(matrix4f, x1, y1, 0.0F).color(0x50, 0x50, 0x50, 0x90).next();
+			fillerBuffer.vertex(matrix4f, x1, y0, 0.0F).color(0x50, 0x50, 0x50, 0x90).next();
+			fillerBuffer.vertex(matrix4f, 1, y0, 0.0F).color(0x50, 0x50, 0x50, 0x90).next();
+
+			textRenderer.draw(string, 2.0F, top, 0xE0E0E0, false, matrix4f, textBuffer, false, 0, 0xF000F0, rightToLeft);
+		}
+
+		final int rightLimit = rightList.size();
+		final int scaleWidth = MinecraftClient.getInstance().getWindow().getScaledWidth() - 2;
+
+		for (int i = 0; i < rightLimit; ++i) {
+			final String string = rightList.get(i);
+
+			if (string == null || string.isEmpty()) {
+				continue;
+			}
+
+			final int width = this.textRenderer.getWidth(string);
+			final int left = scaleWidth - width;
+			final int top = 2 + HEIGHT * i;
+			final int x0 = left - 1;
+			final int x1 = left + width + 1;
+			final int y0 = top - 1;
+			final int y1 = top + HEIGHT - 1;
+
+			fillerBuffer.vertex(matrix4f, x0, y1, 0.0F).color(0x50, 0x50, 0x50, 0x90).next();
+			fillerBuffer.vertex(matrix4f, x1, y1, 0.0F).color(0x50, 0x50, 0x50, 0x90).next();
+			fillerBuffer.vertex(matrix4f, x1, y0, 0.0F).color(0x50, 0x50, 0x50, 0x90).next();
+			fillerBuffer.vertex(matrix4f, x0, y0, 0.0F).color(0x50, 0x50, 0x50, 0x90).next();
+
+			textRenderer.draw(string, left, top, 0xE0E0E0, false, matrix4f, textBuffer, false, 0, 0xF000F0, rightToLeft);
+		}
+
+		fillerBuffer.end();
+		RenderSystem.enableBlend();
+		RenderSystem.disableTexture();
+		RenderSystem.defaultBlendFunc();
+		RenderSystem.setShader(GameRenderer::getPositionColorShader);
+		BufferRenderer.draw(fillerBuffer);
+		RenderSystem.enableTexture();
+		RenderSystem.disableBlend();
+		textBuffer.draw();
+
+		leftList = null;
+		rightList = null;
+	}
 
 	@Inject(method = "getLeftText", at = @At("RETURN"), cancellable = false, require = 1)
 	private void onGetLeftText(CallbackInfoReturnable<List<String>> ci) {
@@ -52,18 +150,5 @@ public class MixinDebugHud extends DrawableHelper {
 		list.add(TransferBufferAllocator.debugString());
 		list.add(GlBufferAllocator.debugString());
 		list.add(ArrayVertexCollector.debugReport());
-	}
-
-	@Inject(method = "renderLeftText", at = @At("HEAD"), cancellable = false, require = 1)
-	private void beforeRenderLeftText(MatrixStack matrixStack, CallbackInfo ci) {
-		final Matrix4f matrix4f = matrixStack.peek().getModel();
-		DrawBetterer.beginBatchFill(matrix4f);
-		((TextRendererExt) textRenderer).canvas_beginBatchDraw(matrix4f);
-	}
-
-	@Inject(method = "renderRightText", at = @At("RETURN"), cancellable = false, require = 1)
-	private void afterRenderRightText(MatrixStack matrixStack, CallbackInfo ci) {
-		DrawBetterer.endBatchFile();
-		((TextRendererExt) textRenderer).canvas_endBatchDraw();
 	}
 }
