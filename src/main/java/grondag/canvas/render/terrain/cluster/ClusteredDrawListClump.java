ffa492d2d3a1419ba216e2ca1e5fea98b44b8810
grondag <>
2021-07-18 12:14:16 -0700

    Stub in clustered terrain config

---
+++ src/main/java/grondag/canvas/render/terrain/cluster/ClusteredDrawListClump.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 2019, 2020 grondag
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License.  You may obtain a copy
+ *  of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+
+package grondag.canvas.render.terrain.cluster;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+
+import net.minecraft.client.render.VertexFormat.DrawMode;
+
+import grondag.canvas.varia.GFX;
+
+public class ClusteredDrawListClump {
+	protected final ObjectArrayList<ClusteredDrawableStorage> stores = new ObjectArrayList<>();
+
+	public void draw(int elementType) {
+		final int limit = stores.size();
+
+		for (int regionIndex = 0; regionIndex < limit; ++regionIndex) {
+			ClusteredDrawableStorage store = stores.get(regionIndex);
+			GFX.drawElementsBaseVertex(DrawMode.QUADS.mode, store.triVertexCount, elementType, 0L, store.baseVertex());
+		}
+	}
+
+	public void add(ClusteredDrawableStorage storage) {
+		stores.add(storage);
+	}
+
+	public void bind() {
+		stores.get(0).getClump().bind();
+	}
+}
