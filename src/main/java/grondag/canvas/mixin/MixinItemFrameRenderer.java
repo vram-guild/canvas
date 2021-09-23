1a2227e1e71ce835562203d4ad2768c6d483cc2a
grondag <>
2021-09-22 20:26:09 -0700

    Fix mojmap leftover

--- src/main/java/grondag/canvas/mixin/MixinItemFrameRenderer.java
+++ src/main/java/grondag/canvas/mixin/MixinItemFrameRenderer.java
@@ -16,40 +16,38 @@
 
 package grondag.canvas.mixin;
 
-//import com.mojang.blaze3d.vertex.PoseStack;
-//import com.mojang.blaze3d.vertex.VertexConsumer;
-//import org.jetbrains.annotations.Nullable;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Redirect;
-//
-//import net.minecraft.client.renderer.MultiBufferSource;
-//import net.minecraft.client.renderer.block.ModelBlockRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.renderer.entity.ItemFrameRenderer;
-//import net.minecraft.client.renderer.texture.OverlayTexture;
-//import net.minecraft.client.resources.model.BakedModel;
-//import net.minecraft.world.entity.decoration.ItemFrame;
-//import net.minecraft.world.level.block.state.BlockState;
-//
-//import grondag.canvas.apiimpl.rendercontext.EntityBlockRenderContext;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.level.block.state.BlockState;
+
+import grondag.canvas.apiimpl.rendercontext.EntityBlockRenderContext;
 
 @Mixin(ItemFrameRenderer.class)
 public abstract class MixinItemFrameRenderer {
-	// WIP: make this work again
-	//@Redirect(method = "Lnet/minecraft/client/renderer/entity/ItemFrameRenderer;render(Lnet/minecraft/world/entity/decoration/ItemFrame;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I))V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/ModelBlockRenderer;renderModel(Lcom/mojang/blaze3d/vertex/PoseStack$Pose;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/client/resources/model/BakedkModel;FFFII)V"))
-	//private void onRender(
-	//	// these are the redirect params
-	//	ModelBlockRenderer renderer,
-	//	PoseStack.Pose entry,
-	//	VertexConsumer vertexConsumer,
-	//	@Nullable BlockState blockState,
-	//	BakedModel bakedModel,
-	//	float green, float red, float blue, int light, int overlay,
-	//	// these are the locals
-	//	ItemFrame itemFrameEntity,
-	//	float f, float g, PoseStack matrixStack, MultiBufferSource vertexConsumerProvider, int i
-	//) {
-	//	EntityBlockRenderContext.get().renderItemFrame(renderer, bakedModel, matrixStack, vertexConsumerProvider, OverlayTexture.NO_OVERLAY, light, itemFrameEntity);
-	//}
+	@Redirect(method = "Lnet/minecraft/client/renderer/entity/ItemFrameRenderer;render(Lnet/minecraft/world/entity/decoration/ItemFrame;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/ModelBlockRenderer;renderModel(Lcom/mojang/blaze3d/vertex/PoseStack$Pose;Lcom/mojang/blaze3d/vertex/VertexConsumer;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/client/resources/model/BakedModel;FFFII)V"))
+	private void onRender(
+		// these are the redirect params
+		ModelBlockRenderer renderer,
+		PoseStack.Pose entry,
+		VertexConsumer vertexConsumer,
+		@Nullable BlockState blockState,
+		BakedModel bakedModel,
+		float green, float red, float blue, int light, int overlay,
+		// these are the locals
+		ItemFrame itemFrameEntity,
+		float f, float g, PoseStack matrixStack, MultiBufferSource vertexConsumerProvider, int i
+	) {
+		EntityBlockRenderContext.get().renderItemFrame(renderer, bakedModel, matrixStack, vertexConsumerProvider, OverlayTexture.NO_OVERLAY, light, itemFrameEntity);
+	}
 }
