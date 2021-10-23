c01a7b3843f8f02f19f473157c877e4c3433969e
grondag <grondag@users.noreply.github.com>
2021-10-23 16:38:23 -0700

    Use new vertex bucket sorter

--- src/main/java/grondag/canvas/buffer/input/CompoundVertexCollector.java
+++ src/main/java/grondag/canvas/buffer/input/CompoundVertexCollector.java
@@ -20,45 +20,121 @@
 
 package grondag.canvas.buffer.input;
 
+import java.nio.IntBuffer;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.world.phys.Vec3;
+
 import io.vram.frex.api.material.RenderMaterial;
 import io.vram.frex.base.renderer.mesh.BaseQuadView;
 
 import grondag.canvas.buffer.format.CanvasVertexFormats;
+import grondag.canvas.buffer.render.TransferBuffer;
 import grondag.canvas.material.state.RenderState;
 import grondag.canvas.render.terrain.TerrainFormat;
+import grondag.canvas.render.terrain.TerrainSectorMap.RegionRenderSector;
 
-public class CompoundVertexCollector extends ArrayVertexCollector implements DrawableVertexCollector {
-	public final boolean isTerrain;
-	private final BucketSorter bucketSorter;
+public class CompoundVertexCollector extends BaseVertexCollector {
+	private final VertexBucketFunction bucketFunction;
+	private final int bucketCount;
+	private final SimpleVertexCollector[] collectors;
 
 	public CompoundVertexCollector(RenderState renderState, boolean isTerrain, int[] target) {
 		super(renderState, isTerrain ? TerrainFormat.TERRAIN_MATERIAL.quadStrideInts : CanvasVertexFormats.STANDARD_MATERIAL_FORMAT.quadStrideInts, target);
-		this.isTerrain = isTerrain;
-		bucketSorter = isTerrain ? new BucketSorter() : null;
+
+		// WIP add support for entity pass and for shadow on terrain
+		assert isTerrain;
+		bucketCount = VertexBucketFunction.TERRAIN_BUCKET_COUNT;
+		bucketFunction = VertexBucketFunction.TERRAIN;
+
+		collectors = new SimpleVertexCollector[bucketCount];
+
+		for (int i = 0; i < bucketCount; ++i) {
+			collectors[i] = new SimpleVertexCollector(renderState, isTerrain, target);
+		}
 	}
 
 	@Override
 	public void commit(BaseQuadView quad, RenderMaterial mat) {
-		assert isTerrain;
-
-		if (bucketSorter != null) {
-			bucketSorter.add(quad.effectiveCullFaceId(), integerSize);
-		}
-
-		commit(quadStrideInts);
+		collectors[bucketFunction.computeBucket(quad, mat)].commit();
+		integerSize += quadStrideInts;
 	}
 
 	@Override
 	public final void clear() {
-		super.clear();
+		integerSize = 0;
 
-		if (bucketSorter != null) {
-			bucketSorter.clear();
+		for (int i = 0; i < bucketCount; ++i) {
+			collectors[i].clear();
 		}
 	}
 
 	@Override
 	public VertexBucket[] vertexBuckets() {
-		return bucketSorter == null ? null : bucketSorter.sort(vertexData, integerSize);
+		final VertexBucket[] result = new VertexBucket[bucketCount];
+		int index = 0;
+
+		for (int i = 0; i < bucketCount; ++i) {
+			final int vertexCount = collectors[i].vertexCount();
+			result[i] = new VertexBucket(index, vertexCount);
+			index += vertexCount;
+		}
+
+		return result;
+	}
+
+	@Override
+	public void commit(int size) {
+		throw new UnsupportedOperationException("Commit on compount collector must provide quad and material");
+	}
+
+	@Override
+	public void toBuffer(IntBuffer intBuffer, int targetIndex) {
+		for (int i = 0; i < bucketCount; ++i) {
+			final var c = collectors[i];
+
+			if (!c.isEmpty()) {
+				collectors[i].toBuffer(intBuffer, targetIndex);
+				targetIndex += collectors[i].integerSize;
+			}
+		}
+	}
+
+	@Override
+	public void toBuffer(TransferBuffer targetBuffer, int bufferTargetIndex) {
+		for (int i = 0; i < bucketCount; ++i) {
+			final var c = collectors[i];
+
+			if (!c.isEmpty()) {
+				collectors[i].toBuffer(targetBuffer, bufferTargetIndex);
+				bufferTargetIndex += collectors[i].integerSize;
+			}
+		}
+	}
+
+	@Override
+	public void sortIfNeeded() {
+		// NOOP
+	}
+
+	@Override
+	public boolean sorted() {
+		return false;
+	}
+
+	@Override
+	public boolean sortTerrainQuads(Vec3 sortPos, RegionRenderSector sector) {
+		throw new UnsupportedOperationException("Compound vertex collector does not support sortTerrainQuads.");
+	}
+
+	@Override
+	public @Nullable int[] saveState(@Nullable int[] translucentState) {
+		throw new UnsupportedOperationException("Compound vertex collector does not support saveState.");
+	}
+
+	@Override
+	public void loadState(int[] state) {
+		throw new UnsupportedOperationException("Compound vertex collector does not support loadState");
 	}
 }
