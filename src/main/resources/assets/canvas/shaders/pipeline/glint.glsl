f2dc7ddf2a97bdba703c12c52eea22e31ac7d7ca
spiralhalo <re.nanashi95@gmail.com>
2021-12-26 13:20:56 +0700

    Make enchantment glint rendering just like vanilla

--- src/main/resources/assets/canvas/shaders/pipeline/glint.glsl
+++ src/main/resources/assets/canvas/shaders/pipeline/glint.glsl
@@ -12,11 +12,31 @@ uniform sampler2D cvu_glint;
 
 void glintify(inout vec4 a, float glint) {
 	if (glint == 1.0) {
-		float t = frx_renderSeconds * 0.25;
-		vec2 tt = vec2(-t * 0.25, t);
-		vec2 uv = frx_normalizeMappedUV(frx_texcoord) * 0.125;
+		// vanilla scale factor for entity, works in most scenario
+		const float scale = 0.16;
 
-		a += texture(cvu_glint, uv + tt);
-		a = clamp(a, 0.0, 1.0);
+		// vanilla rotation factor
+		const float angle = PI * 10. / 180.;
+		const vec3  axis = vec3(0.0, 0.0, 1.0);
+		const float s = sin(angle);
+		const float c = cos(angle);
+		const float oc = 1.0 - c;
+		const mat4 rotation = mat4(
+		oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+		oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+		oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+		0.0,                                0.0,                                0.0,                                1.0);
+
+		// vanilla translation factor
+		float time = frx_renderSeconds * 8.;
+		float tx = mod(time, 110.) / 110.;
+		float ty = mod(time, 30.) / 30.;
+		vec2 translation = vec2(-tx, ty);
+
+		vec2 uv = (rotation * vec4(frx_normalizeMappedUV(frx_texcoord) * scale, 0.0, 1.0)).xy + translation;
+		vec4 glint = vec4(texture(cvu_glint, uv).rgb, 0.0);
+
+		// emulate GL_SRC_COLOR sfactor
+		a = clamp(a + glint * glint, 0.0, 1.0);
 	}
 }
