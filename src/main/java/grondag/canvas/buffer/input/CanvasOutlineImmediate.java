84383a8eaf222257f0d843bef101632dbc759e52
spiralhalo <re.nanashi95@gmail.com>
2022-01-13 22:11:11 +0700

    checkstyle

--- src/main/java/grondag/canvas/buffer/input/CanvasOutlineImmediate.java
+++ src/main/java/grondag/canvas/buffer/input/CanvasOutlineImmediate.java
@@ -14,64 +14,64 @@ import net.minecraft.client.renderer.RenderType;
 
 // This class can't extend CanvasImmediate because item renderer treats it as special case
 public class CanvasOutlineImmediate implements MultiBufferSource {
-    private final CanvasImmediate mainImmediate;
-    private final CanvasImmediate auxImmediate;
+	private final CanvasImmediate mainImmediate;
+	private final CanvasImmediate auxImmediate;
 
-    private int outlineRed = 255;
-    private int outlineGreen = 255;
-    private int outlineBlue = 255;
-    private int outlineAlpha = 255;
+	private int outlineRed = 255;
+	private int outlineGreen = 255;
+	private int outlineBlue = 255;
+	private int outlineAlpha = 255;
 
-    public CanvasOutlineImmediate(CanvasImmediate mainImmediate) {
-        this.mainImmediate = mainImmediate;
-        this.auxImmediate = new CanvasImmediate(new BufferBuilder(256), ImmutableMap.of(), mainImmediate.contextState);
-    }
+	public CanvasOutlineImmediate(CanvasImmediate mainImmediate) {
+		this.mainImmediate = mainImmediate;
+		this.auxImmediate = new CanvasImmediate(new BufferBuilder(256), ImmutableMap.of(), mainImmediate.contextState);
+	}
 
-    @Override
-    public VertexConsumer getBuffer(RenderType renderType) {
-        VertexConsumer auxConsumer;
-        if (renderType.isOutline()) {
-            auxConsumer = auxImmediate.getBuffer(renderType);
-            return new EntityOutlineGenerator(auxConsumer, outlineRed, outlineGreen, outlineBlue, outlineAlpha);
-        } else {
-            Optional<RenderType> optionalOutline = renderType.outline();
-            if (optionalOutline.isPresent()) {
-                VertexConsumer mainConsumer = this.mainImmediate.getBuffer(renderType);
-                auxConsumer = auxImmediate.getBuffer(optionalOutline.get());
-                EntityOutlineGenerator entityOutlineGenerator = new EntityOutlineGenerator(auxConsumer, outlineRed, outlineGreen, outlineBlue, 255);
-                return new FixedDouble(entityOutlineGenerator, mainConsumer);
-            } else {
-                return this.mainImmediate.getBuffer(renderType);
-            }
-        }
-    }
+	@Override
+	public VertexConsumer getBuffer(RenderType renderType) {
+		if (renderType.isOutline()) {
+			final VertexConsumer auxConsumer = auxImmediate.getBuffer(renderType);
+			return new EntityOutlineGenerator(auxConsumer, outlineRed, outlineGreen, outlineBlue, outlineAlpha);
+		} else {
+			Optional<RenderType> optionalOutline = renderType.outline();
 
-    public void setColor(int red, int green, int blue, int alpha) {
-        outlineRed = red;
-        outlineGreen = green;
-        outlineBlue = blue;
-        outlineAlpha = alpha;
-    }
+			if (optionalOutline.isPresent()) {
+				final VertexConsumer mainConsumer = this.mainImmediate.getBuffer(renderType);
+				final VertexConsumer auxConsumer = auxImmediate.getBuffer(optionalOutline.get());
+				final EntityOutlineGenerator entityOutlineGenerator = new EntityOutlineGenerator(auxConsumer, outlineRed, outlineGreen, outlineBlue, 255);
+				return new FixedDouble(entityOutlineGenerator, mainConsumer);
+			} else {
+				return this.mainImmediate.getBuffer(renderType);
+			}
+		}
+	}
 
-    public void endOutlineBatch() {
-        auxImmediate.endBatch();
-    }
+	public void setColor(int red, int green, int blue, int alpha) {
+		outlineRed = red;
+		outlineGreen = green;
+		outlineBlue = blue;
+		outlineAlpha = alpha;
+	}
 
-    private static class FixedDouble extends VertexMultiConsumer.Double {
-        private final VertexConsumer a, b;
+	public void endOutlineBatch() {
+		auxImmediate.endBatch();
+	}
 
-        public FixedDouble(VertexConsumer vertexConsumer, VertexConsumer vertexConsumer2) {
-            super(vertexConsumer, vertexConsumer2);
-            a = vertexConsumer;
-            b = vertexConsumer2;
-        }
+	private static class FixedDouble extends VertexMultiConsumer.Double {
+		private final VertexConsumer a, b;
 
-        // this fixes lighting on items that has outline. Don't ask me why
-        @Override
-        public VertexConsumer uv2(int i) {
-            a.uv2(i);
-            b.uv2(i);
-            return this;
-        }
-    }
+		FixedDouble(VertexConsumer vertexConsumer, VertexConsumer vertexConsumer2) {
+			super(vertexConsumer, vertexConsumer2);
+			a = vertexConsumer;
+			b = vertexConsumer2;
+		}
+
+		// This fixes lighting on items that have outline. Don't ask me why
+		@Override
+		public VertexConsumer uv2(int i) {
+			a.uv2(i);
+			b.uv2(i);
+			return this;
+		}
+	}
 }
