e3172c5bb6bf4c7e6cf5220d479ddcbfdb92c41c
spiralhalo <re.nanashi95@gmail.com>
2022-05-19 23:20:56 +0700

    Revert "Simplify" - Actually wants to invalidate view proper
    This reverts commit 6f1b50b7ee0676f387107e6ee1a540a62ee63237.

--- src/main/java/grondag/canvas/terrain/occlusion/shadow/ShadowOccluder.java
+++ src/main/java/grondag/canvas/terrain/occlusion/shadow/ShadowOccluder.java
@@ -93,16 +93,17 @@ public class ShadowOccluder extends AbstractOccluder {
 		z3 = cascadeCentersAndRadii[14];
 		r3 = cascadeCentersAndRadii[15];
 
+		final boolean invalidateView;
 		if (lastViewVersion == occlusionFrustum.viewVersion()) {
 			final float lightSourceMovement = 1.0f - lastVersionedLightVector.dot(ShaderDataManager.skyLightVector);
 			// big sun movement, about 12 in-game minutes or 200 ticks
-			if(lightSourceMovement > 0.0025f) {
-				lastVersionedLightVector.load(ShaderDataManager.skyLightVector);
-				invalidate();
-			}
+			invalidateView = lightSourceMovement > 0.0025f;
+		} else {
+			invalidateView = false;
 		}
 
-		lastViewVersion = occlusionFrustum.viewVersion();
+		if (invalidateView) lastVersionedLightVector.load(ShaderDataManager.skyLightVector);
+		lastViewVersion = invalidateView ? -1 : occlusionFrustum.viewVersion();
 		lastCameraPos = occlusionFrustum.lastCameraPos();
 	}
 
