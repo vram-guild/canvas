4d56bdb6f52c312728fc2f3dc742e55bbf903a1a
grondag <grondag@users.noreply.github.com>
2022-05-27 18:46:15 -0700

    Update upload script

--- upload.sh
+++ upload.sh
@@ -1,11 +1,11 @@
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   echo "Uploading to mod distribution sites"
   cd fabric
-  ../gradlew curseforge publishModrinth
+  ../gradlew curseforge modrinth --rerun-tasks
   cd ..
 
   cd forge
-  ../gradlew curseforge publishModrinth
+  ../gradlew curseforge modrinth --rerun-tasks
   cd ..
 else
   echo "Git has uncommitted changes - upload to mod distribution sites cancelled."
