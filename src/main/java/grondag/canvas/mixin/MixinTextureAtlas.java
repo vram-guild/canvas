vram-guild/canvas

commit 083c8899a543c0102563df073738db42f1f90e6e

Doing my best to fix sprite animation tracking

src/main/java/grondag/canvas/mixin/MixinTextureAtlas.java
@@ -23,7 +23,6 @@ package grondag.canvas.mixin;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.BitSet;
-import java.util.List;
 import java.util.function.BooleanSupplier;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -38,18 +37,17 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.AbstractTexture;
+import net.minecraft.client.renderer.texture.SpriteLoader;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
-
-import io.vram.frex.impl.texture.TextureAtlasPreparationExt;
 
 import grondag.canvas.CanvasMod;
 import grondag.canvas.apiimpl.rendercontext.CanvasBlockRenderContext;
 import grondag.canvas.apiimpl.rendercontext.CanvasItemRenderContext;
 import grondag.canvas.config.Configurator;
 import grondag.canvas.material.state.TerrainRenderStates;
-import grondag.canvas.mixinterface.SpriteExt;
+import grondag.canvas.mixinterface.SpriteContentsExt;
 import grondag.canvas.mixinterface.TextureAtlasExt;
 import grondag.canvas.render.world.CanvasWorldRenderer;
 
@@ -61,22 +59,20 @@ public abstract class MixinTextureAtlas extends AbstractTexture implements Textu
 	private final BitSet animationBits = new BitSet();
 	private final BitSet perFrameBits = new BitSet();
 
-	@Inject(at = @At("HEAD"), method = "reload")
-	private void beforeReload(TextureAtlas.Preparations input, CallbackInfo ci) {
+	@Inject(at = @At("HEAD"), method = "upload")
+	private void beforeReload(SpriteLoader.Preparations preparations, CallbackInfo ci) {
 		if (Configurator.traceTextureLoad) {
 			CanvasMod.LOG.info("Start of reload for atlas " + location.toString());
 		}
 
-		final var dataExt = (TextureAtlasPreparationExt) input;
-		width = dataExt.frex_atlasWidth();
-		height = dataExt.frex_atlasHeight();
+		width = preparations.width();
+		height = preparations.height();
 
 		int animationIndex = 0;
-		final List<TextureAtlasSprite> sprites = dataExt.frex_sprites();
 
-		for (final TextureAtlasSprite sprite : sprites) {
-			if (sprite.getAnimationTicker() != null) {
-				final var spriteExt = (SpriteExt) sprite;
+		for (final TextureAtlasSprite sprite : preparations.regions().values()) {
+			final var spriteExt = (SpriteContentsExt) sprite.contents();
+			if (!spriteExt.canvas_isAnimated()) {
 				final int instanceIndex = animationIndex;
 				final BooleanSupplier getter = () -> animationBits.get(instanceIndex);
 				spriteExt.canvas_initializeAnimation(getter, instanceIndex);
@@ -88,8 +84,8 @@ public abstract class MixinTextureAtlas extends AbstractTexture implements Textu
 		animationBits.set(0, animationIndex);
 	}
 
-	@Inject(at = @At("RETURN"), method = "reload")
-	private void afterReload(TextureAtlas.Preparations input, CallbackInfo ci) {
+	@Inject(at = @At("RETURN"), method = "upload")
+	private void afterReload(SpriteLoader.Preparations preparations, CallbackInfo ci) {
 		if (Configurator.debugSpriteAtlas) {
 			outputAtlasImage();
 		}

fabric/src/main/resources/canvas.accesswidener
@@ -140,16 +140,16 @@ accessible class com/mojang/blaze3d/vertex/VertexMultiConsumer$Double
 
 accessible method net/minecraft/client/renderer/culling/Frustum calculateFrustum (Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;)V
 
-accessible class net/minecraft/client/renderer/texture/TextureAtlasSprite$InterpolationData
-accessible class net/minecraft/client/renderer/texture/TextureAtlasSprite$AnimatedTexture
+accessible class net/minecraft/client/renderer/texture/SpriteContents$InterpolationData
+accessible class net/minecraft/client/renderer/texture/SpriteContents$AnimatedTexture
 
-accessible class net/minecraft/client/renderer/texture/TextureAtlasSprite$FrameInfo
-accessible field net/minecraft/client/renderer/texture/TextureAtlasSprite$FrameInfo index I
-accessible field net/minecraft/client/renderer/texture/TextureAtlasSprite$FrameInfo time I
+accessible class net/minecraft/client/renderer/texture/SpriteContents$FrameInfo
+accessible field net/minecraft/client/renderer/texture/SpriteContents$FrameInfo index I
+accessible field net/minecraft/client/renderer/texture/SpriteContents$FrameInfo time I
+
+accessible class net/minecraft/client/renderer/texture/SpriteContents$Ticker
 
 accessible class net/minecraft/client/renderer/texture/Stitcher$Holder
-
-accessible field net/minecraft/client/renderer/texture/TextureAtlasSprite$Info metadata Lnet/minecraft/client/resources/metadata/animation/AnimationMetadataSection;
 
 accessible field net/minecraft/client/renderer/entity/EntityRenderDispatcher SHADOW_RENDER_TYPE Lnet/minecraft/client/renderer/RenderType;
 

src/main/java/grondag/canvas/apiimpl/rendercontext/encoder/BaseQuadEncoder.java
@@ -28,6 +28,7 @@ import io.vram.frex.api.model.InputContext;
 import io.vram.frex.base.renderer.mesh.BaseQuadEmitter;
 
 import grondag.canvas.buffer.input.VertexCollectorList;
+import grondag.canvas.mixinterface.SpriteContentsExt;
 import grondag.canvas.mixinterface.SpriteExt;
 
 public abstract class BaseQuadEncoder {
@@ -48,7 +49,7 @@ public abstract class BaseQuadEncoder {
 
 		if (!mat.discardsTexture() && mat.texture().isAtlas()) {
 			// WIP: create and use sprite method on quad
-			final int animationIndex = ((SpriteExt) mat.texture().spriteIndex().fromIndex(quad.spriteId())).canvas_animationIndex();
+			final int animationIndex = ((SpriteContentsExt) mat.texture().spriteIndex().fromIndex(quad.spriteId()).contents()).canvas_animationIndex();
 
 			if (animationIndex >= 0) {
 				animationBits.set(animationIndex);

src/main/java/grondag/canvas/material/state/CanvasRenderMaterial.java
@@ -37,6 +37,7 @@ import io.vram.frex.base.renderer.material.BaseRenderMaterial;
 import io.vram.frex.base.renderer.util.ResourceCache;
 
 import grondag.canvas.material.property.TextureMaterialState;
+import grondag.canvas.mixinterface.SpriteContentsExt;
 import grondag.canvas.mixinterface.SpriteExt;
 import grondag.canvas.mixinterface.TextureAtlasExt;
 import grondag.canvas.shader.MaterialShaderIndexer;
@@ -129,7 +130,7 @@ public class CanvasRenderMaterial extends BaseRenderMaterial {
 	public void trackPerFrameAnimation(int spriteId) {
 		if (!this.discardsTexture() && texture().isAtlas()) {
 			// WIP: create and use sprite method on quad
-			final int animationIndex = ((SpriteExt) texture().spriteIndex().fromIndex(spriteId)).canvas_animationIndex();
+			final int animationIndex = ((SpriteContentsExt) texture().spriteIndex().fromIndex(spriteId).contents()).canvas_animationIndex();
 
 			if (animationIndex > 0) {
 				((TextureAtlasExt) texture().textureAsAtlas()).canvas_trackFrameAnimation(animationIndex);

src/main/java/grondag/canvas/mixin/MixinAnimatedTexture.java
@@ -31,35 +31,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.renderer.texture.SpriteContents;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 
 import grondag.canvas.mixinterface.AnimatedTextureExt;
+import grondag.canvas.mixinterface.SpriteContentsExt;
 import grondag.canvas.mixinterface.SpriteExt;
 
-@Mixin(TextureAtlasSprite.AnimatedTexture.class)
+@Mixin(SpriteContents.AnimatedTexture.class)
 public class MixinAnimatedTexture implements AnimatedTextureExt {
 	@Shadow(aliases = {"this$0", "a", "field_28469"})
-	@Dynamic private TextureAtlasSprite parent;
-
-	@Shadow int frame;
-	@Shadow int subFrame;
-	@Shadow @Final List<TextureAtlasSprite.FrameInfo> frames;
+	@Dynamic private SpriteContents parent;
+	@Shadow @Final List<SpriteContents.FrameInfo> frames;
 	@Shadow @Final private int frameRowSize;
-
-	@Nullable
-	@Shadow @Final private TextureAtlasSprite.InterpolationData interpolationData;
-
-	@Inject(method = "tick", at = @At("HEAD"), cancellable = true)
-	private void beforeTick(CallbackInfo ci) {
-		if (!((SpriteExt) parent).canvas_shouldAnimate()) {
-			ci.cancel();
-		}
-	}
-
-	@Override
-	public TextureAtlasSprite.InterpolationData canvas_interpolation() {
-		return interpolationData;
-	}
 
 	@Override
 	public int canvas_frameCount() {
@@ -67,17 +51,7 @@ public class MixinAnimatedTexture implements AnimatedTextureExt {
 	}
 
 	@Override
-	public int canvas_frameIndex() {
-		return frame;
-	}
-
-	@Override
-	public int canvas_frameTicks() {
-		return subFrame;
-	}
-
-	@Override
-	public List<TextureAtlasSprite.FrameInfo> canvas_frames() {
+	public List<SpriteContents.FrameInfo> canvas_frames() {
 		return frames;
 	}
 }

src/main/java/grondag/canvas/mixin/MixinAnimatedTextureTicker.java
@@ -1,0 +1,67 @@
+package grondag.canvas.mixin;
+
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.renderer.texture.SpriteContents;
+
+import grondag.canvas.mixinterface.AnimatedTextureExt;
+import grondag.canvas.mixinterface.AnimatedTextureTickerExt;
+import grondag.canvas.mixinterface.SpriteContentsExt;
+
+@Mixin(SpriteContents.Ticker.class)
+public class MixinAnimatedTextureTicker implements AnimatedTextureTickerExt {
+	@Shadow(aliases = {"this$0", "a", "field_40543"})
+	@Dynamic
+	private SpriteContents parent;
+	@Shadow
+	int frame;
+	@Shadow int subFrame;
+
+	@Nullable
+	@Shadow @Final
+	private SpriteContents.InterpolationData interpolationData;
+
+	@Shadow @Final private SpriteContents.AnimatedTexture animationInfo;
+
+	@Override
+	public int canvas_frameIndex() {
+		return frame;
+	}
+
+	@Override
+	public int canvas_frameTicks() {
+		return subFrame;
+	}
+
+
+	@Inject(method = "tickAndUpload", at = @At("HEAD"), cancellable = true)
+	private void beforeTick(CallbackInfo ci) {
+		if (!((SpriteContentsExt) parent).canvas_shouldAnimate()) {
+			ci.cancel();
+		}
+	}
+
+	@Override
+	public SpriteContents.InterpolationData canvas_interpolation() {
+		return interpolationData;
+	}
+
+	@Override
+	public int canvas_frameCount() {
+		return ((AnimatedTextureExt) animationInfo).canvas_frameCount();
+	}
+
+	@Override
+	public List<SpriteContents.FrameInfo> canvas_frames() {
+		return ((AnimatedTextureExt) animationInfo).canvas_frames();
+	}
+}

src/main/java/grondag/canvas/mixin/MixinAnimationMetadataSection.java
@@ -39,18 +39,23 @@ import grondag.canvas.mixinterface.SimulatedFrame;
 
 @Mixin(AnimationMetadataSection.class)
 public class MixinAnimationMetadataSection implements AnimationMetadataSectionExt {
-	@Shadow private List<AnimationFrame> frames;
 	@Shadow private int frameWidth;
 	@Shadow private int frameHeight;
-	@Shadow private int defaultFrameTime;
-	@Shadow private boolean interpolatedFrames;
 
 	private int pngWidth, pngHeight;
 
-	@Inject(method = "getFrameSize", at = @At("HEAD"))
+	@Inject(method = "calculateFrameSize", at = @At("HEAD"))
 	private void beforeGetFrameSize(int pngWidth, int pngHeight, CallbackInfoReturnable<Pair<Integer, Integer>> ci) {
 		this.pngWidth = pngWidth;
 		this.pngHeight = pngHeight;
+	}
+
+	public int getFrameHeight(int defHeight) {
+		return frameHeight == -1 ? defHeight : frameHeight;
+	}
+
+	public int getFrameWidth(int defWidth) {
+		return frameWidth == -1 ? defWidth : frameWidth;
 	}
 
 	@Override
@@ -58,8 +63,8 @@ public class MixinAnimationMetadataSection implements AnimationMetadataSectionEx
 		// forecasts the outcome of Sprite.createAnimation()
 
 		final AnimationMetadataSection animationMetadata = (AnimationMetadataSection) (Object) this;
-		final int widthFrames = pngWidth / animationMetadata.getFrameWidth(spriteWidth);
-		final int heightFrames = pngHeight / animationMetadata.getFrameHeight(spriteHeight);
+		final int widthFrames = pngWidth / getFrameWidth(spriteWidth);
+		final int heightFrames = pngHeight / getFrameHeight(spriteHeight);
 		final int expectedFrames = widthFrames * heightFrames;
 		final var frames = new ObjectArrayList<SimulatedFrame>();
 

src/main/java/grondag/canvas/mixin/MixinScreenEffectRenderer.java
@@ -29,14 +29,16 @@ import net.minecraft.client.renderer.ScreenEffectRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.ModelBakery;
 
+import grondag.canvas.mixinterface.SpriteContentsExt;
 import grondag.canvas.mixinterface.SpriteExt;
 import grondag.canvas.mixinterface.TextureAtlasExt;
 
 @Mixin(ScreenEffectRenderer.class)
 public class MixinScreenEffectRenderer {
-	@Inject(method = "renderFire", at = @At("RETURN"), cancellable = true)
-	private static void onRenderFire(CallbackInfo ci) {
-		final TextureAtlasSprite sprite = ModelBakery.FIRE_1.sprite();
-		((TextureAtlasExt) sprite.atlas()).canvas_trackFrameAnimation(((SpriteExt) sprite).canvas_animationIndex());
-	}
+	// TODO: re-enable this
+	// @Inject(method = "renderFire", at = @At("RETURN"), cancellable = true)
+	// private static void onRenderFire(CallbackInfo ci) {
+	// 	final TextureAtlasSprite sprite = ModelBakery.FIRE_1.sprite();
+	// 	((TextureAtlasExt) sprite.atlas()).canvas_trackFrameAnimation(((SpriteContentsExt) sprite.contents()).canvas_animationIndex());
+	// }
 }

commit 00a59e8c163015cceeeea5a358c6a5c68a0dd48a

Fix inverted animation tracking set
Caused by commit 083c8899a543c0102563df073738db42f1f90e6e

src/main/java/grondag/canvas/mixin/MixinTextureAtlas.java
@@ -72,7 +72,7 @@ public abstract class MixinTextureAtlas extends AbstractTexture implements Textu
 
 		for (final TextureAtlasSprite sprite : preparations.regions().values()) {
 			final var spriteExt = (SpriteContentsExt) sprite.contents();
-			if (!spriteExt.canvas_isAnimated()) {
+			if (spriteExt.canvas_isAnimated()) {
 				final int instanceIndex = animationIndex;
 				final BooleanSupplier getter = () -> animationBits.get(instanceIndex);
 				spriteExt.canvas_initializeAnimation(getter, instanceIndex);